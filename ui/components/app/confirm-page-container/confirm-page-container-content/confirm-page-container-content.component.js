b24cb656949ff60dc7d465ad05ef853c867ef10c
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:09:33 +0300

    Initialize collectibles-list-test

---
+++ ui/components/app/confirm-page-container/confirm-page-container-content/confirm-page-container-content.component.js
@@ -0,0 +1,163 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { Tabs, Tab } from '../../../ui/tabs';
+import ErrorMessage from '../../../ui/error-message';
+import ActionableMessage from '../../../ui/actionable-message/actionable-message';
+import { PageContainerFooter } from '../../../ui/page-container';
+import { ConfirmPageContainerSummary, ConfirmPageContainerWarning } from '.';
+
+// eslint-disable-next-line prefer-destructuring
+const EIP_1559_V2 = process.env.EIP_1559_V2;
+
+export default class ConfirmPageContainerContent extends Component {
+  static contextTypes = {
+    t: PropTypes.func.isRequired,
+  };
+
+  static propTypes = {
+    action: PropTypes.string,
+    dataComponent: PropTypes.node,
+    detailsComponent: PropTypes.node,
+    errorKey: PropTypes.string,
+    errorMessage: PropTypes.string,
+    hasSimulationError: PropTypes.bool,
+    hideSubtitle: PropTypes.bool,
+    identiconAddress: PropTypes.string,
+    nonce: PropTypes.string,
+    subtitleComponent: PropTypes.node,
+    title: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    titleComponent: PropTypes.node,
+    warning: PropTypes.string,
+    origin: PropTypes.string.isRequired,
+    ethGasPriceWarning: PropTypes.string,
+    // Footer
+    onCancelAll: PropTypes.func,
+    onCancel: PropTypes.func,
+    cancelText: PropTypes.string,
+    onSubmit: PropTypes.func,
+    onConfirmAnyways: PropTypes.func,
+    submitText: PropTypes.string,
+    disabled: PropTypes.bool,
+    hideConfirmAnyways: PropTypes.bool,
+    unapprovedTxCount: PropTypes.number,
+    rejectNText: PropTypes.string,
+    hideTitle: PropTypes.boolean,
+  };
+
+  renderContent() {
+    const { detailsComponent, dataComponent } = this.props;
+
+    if (detailsComponent && dataComponent) {
+      return this.renderTabs();
+    }
+    return detailsComponent || dataComponent;
+  }
+
+  renderTabs() {
+    const { t } = this.context;
+    const { detailsComponent, dataComponent } = this.props;
+
+    return (
+      <Tabs>
+        <Tab
+          className="confirm-page-container-content__tab"
+          name={t('details')}
+        >
+          {detailsComponent}
+        </Tab>
+        <Tab className="confirm-page-container-content__tab" name={t('data')}>
+          {dataComponent}
+        </Tab>
+      </Tabs>
+    );
+  }
+
+  render() {
+    const {
+      action,
+      errorKey,
+      errorMessage,
+      hasSimulationError,
+      title,
+      titleComponent,
+      subtitleComponent,
+      hideSubtitle,
+      identiconAddress,
+      nonce,
+      detailsComponent,
+      dataComponent,
+      warning,
+      onCancelAll,
+      onCancel,
+      cancelText,
+      onSubmit,
+      submitText,
+      disabled,
+      unapprovedTxCount,
+      rejectNText,
+      origin,
+      ethGasPriceWarning,
+      hideTitle,
+      onConfirmAnyways,
+      hideConfirmAnyways,
+    } = this.props;
+
+    const primaryAction = hideConfirmAnyways
+      ? null
+      : {
+          label: this.context.t('tryAnywayOption'),
+          onClick: onConfirmAnyways,
+        };
+
+    return (
+      <div className="confirm-page-container-content">
+        {warning ? <ConfirmPageContainerWarning warning={warning} /> : null}
+        {ethGasPriceWarning && (
+          <ConfirmPageContainerWarning warning={ethGasPriceWarning} />
+        )}
+        {hasSimulationError && (
+          <div className="confirm-page-container-content__error-container">
+            <ActionableMessage
+              type="danger"
+              primaryAction={primaryAction}
+              message={this.context.t('simulationErrorMessage')}
+            />
+          </div>
+        )}
+        <ConfirmPageContainerSummary
+          className={classnames({
+            'confirm-page-container-summary--border':
+              !detailsComponent || !dataComponent,
+          })}
+          action={action}
+          title={title}
+          titleComponent={titleComponent}
+          subtitleComponent={subtitleComponent}
+          hideSubtitle={hideSubtitle}
+          identiconAddress={identiconAddress}
+          nonce={nonce}
+          origin={origin}
+          hideTitle={hideTitle}
+        />
+        {this.renderContent()}
+        {!EIP_1559_V2 && !hasSimulationError && (errorKey || errorMessage) && (
+          <div className="confirm-page-container-content__error-container">
+            <ErrorMessage errorMessage={errorMessage} errorKey={errorKey} />
+          </div>
+        )}
+        <PageContainerFooter
+          onCancel={onCancel}
+          cancelText={cancelText}
+          onSubmit={onSubmit}
+          submitText={submitText}
+          disabled={disabled}
+        >
+          {unapprovedTxCount > 1 ? (
+            <a onClick={onCancelAll}>{rejectNText}</a>
+          ) : null}
+        </PageContainerFooter>
+      </div>
+    );
+  }
+}
