42a40b38152c61b0c3f8c80276c43215f642f16a
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 01:28:36 +0300

    Initialize snap-account-fixture-e2e

---
+++ ui/pages/settings/security-tab/security-tab.container.js
@@ -0,0 +1,84 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'redux';
+import {
+  setFeatureFlag,
+  setIpfsGateway,
+  setParticipateInMetaMetrics,
+  setUseCurrencyRateCheck,
+  setUseMultiAccountBalanceChecker,
+  setUsePhishDetect,
+  setUseTokenDetection,
+  setUseAddressBarEnsResolution,
+  setOpenSeaEnabled,
+  setUseNftDetection,
+  setUse4ByteResolution,
+} from '../../../store/actions';
+import SecurityTab from './security-tab.component';
+
+const mapStateToProps = (state) => {
+  const {
+    appState: { warning },
+    metamask,
+  } = state;
+  const {
+    featureFlags: { showIncomingTransactions } = {},
+    participateInMetaMetrics,
+    usePhishDetect,
+    useTokenDetection,
+    ipfsGateway,
+    useMultiAccountBalanceChecker,
+    useCurrencyRateCheck,
+    useAddressBarEnsResolution,
+    openSeaEnabled,
+    useNftDetection,
+    use4ByteResolution,
+  } = metamask;
+
+  return {
+    warning,
+    showIncomingTransactions,
+    participateInMetaMetrics,
+    usePhishDetect,
+    useTokenDetection,
+    ipfsGateway,
+    useMultiAccountBalanceChecker,
+    useCurrencyRateCheck,
+    useAddressBarEnsResolution,
+    openSeaEnabled,
+    useNftDetection,
+    use4ByteResolution,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setParticipateInMetaMetrics: (val) =>
+      dispatch(setParticipateInMetaMetrics(val)),
+    setShowIncomingTransactionsFeatureFlag: (shouldShow) =>
+      dispatch(setFeatureFlag('showIncomingTransactions', shouldShow)),
+    setUsePhishDetect: (val) => dispatch(setUsePhishDetect(val)),
+    setUseCurrencyRateCheck: (val) => dispatch(setUseCurrencyRateCheck(val)),
+    setUseTokenDetection: (value) => {
+      return dispatch(setUseTokenDetection(value));
+    },
+    setIpfsGateway: (value) => {
+      return dispatch(setIpfsGateway(value));
+    },
+    setUseMultiAccountBalanceChecker: (value) => {
+      return dispatch(setUseMultiAccountBalanceChecker(value));
+    },
+    setUseAddressBarEnsResolution: (value) =>
+      dispatch(setUseAddressBarEnsResolution(value)),
+    setOpenSeaEnabled: (val) => dispatch(setOpenSeaEnabled(val)),
+    setUseNftDetection: (val) => dispatch(setUseNftDetection(val)),
+    setUse4ByteResolution: (value) => {
+      return dispatch(setUse4ByteResolution(value));
+    },
+  };
+};
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps, mapDispatchToProps),
+)(SecurityTab);
