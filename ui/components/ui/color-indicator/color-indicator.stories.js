5cec2865fbfa8796eb2af3e41f9187bfa1ebac5b
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:06:28 +0300

    Initialize snaps-e2e-enumfix

---
+++ ui/components/ui/color-indicator/color-indicator.stories.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { COLORS, SIZES } from '../../../helpers/constants/design-system';
+import ColorIndicator from './color-indicator';
+
+export default {
+  title: 'Components/UI/ColorIndicator',
+  id: __filename,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: SIZES,
+    },
+    type: {
+      control: {
+        type: 'select',
+      },
+      options: ColorIndicator.TYPES,
+    },
+    color: {
+      control: {
+        type: 'select',
+      },
+      options: COLORS,
+    },
+    borderColor: {
+      control: {
+        type: 'select',
+      },
+      options: { NONE: undefined, ...COLORS },
+    },
+  },
+  args: {
+    size: SIZES.LG,
+    type: ColorIndicator.TYPES.FILLED,
+    color: COLORS.PRIMARY_DEFAULT,
+  },
+};
+
+export const DefaultStory = (args) => (
+  <ColorIndicator
+    size={args.size}
+    type={args.type}
+    color={args.color}
+    borderColor={args.borderColor}
+  />
+);
+
+DefaultStory.storyName = 'Default';
+
+export const WithIcon = (args) => (
+  <ColorIndicator
+    size={args.size}
+    type={args.type}
+    color={args.color}
+    iconClassName="fa fa-question"
+    borderColor={args.borderColor}
+  />
+);
---
+++ ui/hooks/gasFeeInput/useMaxPriorityFeePerGasInput.js
@@ -0,0 +1,82 @@
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
+import { checkNetworkAndAccountSupports1559 } from '../../selectors';
+import { isLegacyTransaction } from '../../helpers/utils/transactions.util';
+
+import { hexWEIToDecGWEI } from '../../../shared/lib/transactions-controller-utils';
+import { feeParamsAreCustom, getGasFeeEstimate } from './utils';
+
+const getMaxPriorityFeePerGasFromTransaction = (
+  transaction,
+  gasFeeEstimates,
+) => {
+  if (gasFeeEstimates?.[transaction?.userFeeLevel]) {
+    return gasFeeEstimates[transaction.userFeeLevel]
+      .suggestedMaxPriorityFeePerGas;
+  }
+  const { maxPriorityFeePerGas, maxFeePerGas, gasPrice } =
+    transaction?.txParams || {};
+  return Number(
+    hexWEIToDecGWEI(maxPriorityFeePerGas || maxFeePerGas || gasPrice),
+  );
+};
+
+/**
+ * @typedef {object} MaxPriorityFeePerGasInputReturnType
+ * @property {DecGweiString} [maxPriorityFeePerGas] - the maxPriorityFeePerGas
+ *  input value.
+ * @property {(DecGweiString) => void} setMaxPriorityFeePerGas - state setter
+ *  method to update the maxPriorityFeePerGas.
+ */
+
+/**
+ * @param options
+ * @param options.estimateToUse
+ * @param options.gasEstimateType
+ * @param options.gasFeeEstimates
+ * @param options.transaction
+ * @returns {MaxPriorityFeePerGasInputReturnType}
+ */
+export function useMaxPriorityFeePerGasInput({
+  estimateToUse,
+  gasEstimateType,
+  gasFeeEstimates,
+  transaction,
+}) {
+  const supportsEIP1559 =
+    useSelector(checkNetworkAndAccountSupports1559) &&
+    !isLegacyTransaction(transaction?.txParams);
+
+  const initialMaxPriorityFeePerGas = supportsEIP1559
+    ? getMaxPriorityFeePerGasFromTransaction(transaction, gasFeeEstimates)
+    : 0;
+
+  const [maxPriorityFeePerGas, setMaxPriorityFeePerGas] = useState(() => {
+    if (initialMaxPriorityFeePerGas && feeParamsAreCustom(transaction)) {
+      return initialMaxPriorityFeePerGas;
+    }
+    return null;
+  });
+
+  useEffect(() => {
+    if (supportsEIP1559 && initialMaxPriorityFeePerGas) {
+      setMaxPriorityFeePerGas(initialMaxPriorityFeePerGas);
+    }
+  }, [initialMaxPriorityFeePerGas, setMaxPriorityFeePerGas, supportsEIP1559]);
+
+  const maxPriorityFeePerGasToUse =
+    maxPriorityFeePerGas ??
+    getGasFeeEstimate(
+      'suggestedMaxPriorityFeePerGas',
+      gasFeeEstimates,
+      gasEstimateType,
+      estimateToUse,
+      initialMaxPriorityFeePerGas || 0,
+    );
+
+  return {
+    maxPriorityFeePerGas: maxPriorityFeePerGasToUse,
+    setMaxPriorityFeePerGas,
+  };
+}
