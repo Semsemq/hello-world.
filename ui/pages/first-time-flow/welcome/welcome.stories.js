17a5016eff6f7ee8f9e2a77b8974bc4c1697bc7c
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:11:22 +0300

    Initialize component-async-await

---
+++ ui/pages/first-time-flow/welcome/welcome.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from './welcome.component';
+
+export default {
+  title: 'Pages/FirstTimeFlow/Welcome',
+};
+
+export const DefaultStory = () => {
+  return <Welcome />;
+};
+
+DefaultStory.storyName = 'Default';
