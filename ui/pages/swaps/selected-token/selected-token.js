60978682eea6d9d65202d5d14779192338d2a85d
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:42:45 +0300

    Initialize add-test-ids

---
+++ ui/pages/swaps/selected-token/selected-token.js
@@ -0,0 +1,66 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import {
+  Icon,
+  IconName,
+  IconSize,
+} from '../../../components/component-library';
+import { IconColor } from '../../../helpers/constants/design-system';
+import UrlIcon from '../../../components/ui/url-icon';
+import { I18nContext } from '../../../contexts/i18n';
+
+export default function SelectedToken({ onClick, selectedToken, testId }) {
+  const t = useContext(I18nContext);
+  return (
+    <div
+      className={classnames(
+        'dropdown-search-list',
+        'dropdown-search-list__selector-closed-container',
+        'dropdown-input-pair__selector--closed',
+      )}
+      onClick={onClick}
+    >
+      <div className="dropdown-search-list__selector-closed">
+        {selectedToken?.iconUrl && selectedToken?.symbol && (
+          <UrlIcon
+            url={selectedToken.iconUrl}
+            className="dropdown-search-list__selector-closed-icon"
+            name={selectedToken?.symbol}
+          />
+        )}
+        {!selectedToken?.iconUrl && (
+          <div className="dropdown-search-list__default-dropdown-icon" />
+        )}
+        <div className="dropdown-search-list__labels">
+          <div className="dropdown-search-list__item-labels">
+            <span
+              data-testid={testId}
+              className={classnames(
+                'dropdown-search-list__closed-primary-label',
+                {
+                  'dropdown-search-list__select-default':
+                    !selectedToken?.symbol,
+                },
+              )}
+            >
+              {selectedToken?.symbol || t('swapSelectAToken')}
+            </span>
+          </div>
+        </div>
+      </div>
+      <Icon
+        name={IconName.ArrowDown}
+        size={IconSize.Xs}
+        marginRight={3}
+        color={IconColor.iconAlternative}
+      />
+    </div>
+  );
+}
+
+SelectedToken.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  selectedToken: PropTypes.object.isRequired,
+};
