30fb4749043726e41e28354482e4ef54d94ccb82
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:24:36 +0300

    Initialize fix/network-insufficient-warning

---
+++ ui/components/app/confirm-page-container/confirm-page-container.container.js
@@ -0,0 +1,35 @@
+import { connect } from 'react-redux';
+import {
+  getAccountsWithLabels,
+  getAddressBookEntry,
+  getIsBuyableTransakChain,
+} from '../../../selectors';
+import { showModal } from '../../../store/actions';
+import ConfirmPageContainer from './confirm-page-container.component';
+
+function mapStateToProps(state, ownProps) {
+  const to = ownProps.toAddress;
+  const isBuyableTransakChain = getIsBuyableTransakChain(state);
+
+  const contact = getAddressBookEntry(state, to);
+  return {
+    contact,
+    toName: contact?.name || ownProps.toName,
+    isOwnedAccount: getAccountsWithLabels(state)
+      .map((accountWithLabel) => accountWithLabel.address)
+      .includes(to),
+    to,
+    isBuyableTransakChain,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    showBuyModal: () => dispatch(showModal({ name: 'DEPOSIT_ETHER' })),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ConfirmPageContainer);
