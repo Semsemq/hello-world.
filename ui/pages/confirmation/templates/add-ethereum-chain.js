94627fa609593925a6b7aa17908b5eec910f5e72
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:21:56 +0300

    Initialize feat/add-custom-network-patch

---
+++ ui/pages/confirmation/templates/add-ethereum-chain.js
@@ -0,0 +1,251 @@
+import { ethErrors } from 'eth-rpc-errors';
+import {
+  SEVERITIES,
+  TYPOGRAPHY,
+  TEXT_ALIGN,
+  JUSTIFY_CONTENT,
+  DISPLAY,
+} from '../../../helpers/constants/design-system';
+import fetchWithCache from '../../../helpers/utils/fetch-with-cache';
+
+const UNRECOGNIZED_CHAIN = {
+  id: 'UNRECOGNIZED_CHAIN',
+  severity: SEVERITIES.WARNING,
+  content: {
+    element: 'span',
+    children: {
+      element: 'MetaMaskTranslation',
+      props: {
+        translationKey: 'unrecognizedChain',
+        variables: [
+          {
+            element: 'a',
+            key: 'unrecognizedChainLink',
+            props: {
+              href:
+                'https://metamask.zendesk.com/hc/en-us/articles/360057142392',
+              target: '__blank',
+              tabIndex: 0,
+            },
+            children: {
+              element: 'MetaMaskTranslation',
+              props: {
+                translationKey: 'unrecognizedChainLinkText',
+              },
+            },
+          },
+        ],
+      },
+    },
+  },
+};
+
+const INVALID_CHAIN = {
+  id: 'INVALID_CHAIN',
+  severity: SEVERITIES.DANGER,
+  content: {
+    element: 'span',
+    children: {
+      element: 'MetaMaskTranslation',
+      props: {
+        translationKey: 'mismatchedChain',
+        variables: [
+          {
+            element: 'a',
+            key: 'mismatchedChainLink',
+            props: {
+              href:
+                'https://metamask.zendesk.com/hc/en-us/articles/360057142392',
+              target: '__blank',
+              tabIndex: 0,
+            },
+            children: {
+              element: 'MetaMaskTranslation',
+              props: {
+                translationKey: 'mismatchedChainLinkText',
+              },
+            },
+          },
+        ],
+      },
+    },
+  },
+};
+
+async function getAlerts(pendingApproval) {
+  const alerts = [];
+  const safeChainsList = await fetchWithCache(
+    'https://chainid.network/chains.json',
+  );
+  const matchedChain = safeChainsList.find(
+    (chain) =>
+      chain.chainId === parseInt(pendingApproval.requestData.chainId, 16),
+  );
+  let validated = Boolean(matchedChain);
+
+  const originIsMetaMask = pendingApproval.origin === 'metamask';
+  if (originIsMetaMask && validated) {
+    return [];
+  }
+
+  if (matchedChain) {
+    if (
+      matchedChain.nativeCurrency?.decimals !== 18 ||
+      matchedChain.name.toLowerCase() !==
+        pendingApproval.requestData.chainName.toLowerCase() ||
+      matchedChain.nativeCurrency?.symbol !== pendingApproval.requestData.ticker
+    ) {
+      validated = false;
+    }
+
+    const { origin } = new URL(pendingApproval.requestData.rpcUrl);
+    if (!matchedChain.rpc.map((rpc) => new URL(rpc).origin).includes(origin)) {
+      validated = false;
+    }
+  }
+
+  if (!matchedChain) {
+    alerts.push(UNRECOGNIZED_CHAIN);
+  } else if (!validated) {
+    alerts.push(INVALID_CHAIN);
+  }
+  return alerts;
+}
+
+function getValues(pendingApproval, t, actions) {
+  const originIsMetaMask = pendingApproval.origin === 'metamask';
+  console.log('pendingApproval', pendingApproval)
+  return {
+    content: [
+      {
+        hide: !originIsMetaMask,
+        element: 'Box',
+        key: 'network-box',
+        props: {
+          textAlign: TEXT_ALIGN.CENTER,
+          display: DISPLAY.FLEX,
+          justifyContent: JUSTIFY_CONTENT.CENTER,
+          marginTop: 4,
+          marginBottom: 2,
+        },
+        children: [
+          {
+            element: 'Chip',
+            key: 'network-chip',
+            props: {
+              label: 'Test',
+              backgroundColor: 'Grey-000',
+              leftIconUrl: './images/avax-token.png',
+            },
+          },
+        ],
+      },
+      {
+        element: 'Typography',
+        key: 'title',
+        children: originIsMetaMask ? "Want to add this network?" : t('addEthereumChainConfirmationTitle'),
+        props: {
+          variant: TYPOGRAPHY.H3,
+          align: 'center',
+          fontWeight: 'bold',
+          boxProps: {
+            margin: [0, 0, 4],
+          },
+        },
+      },
+      {
+        element: 'Typography',
+        key: 'description',
+        children: t('addEthereumChainConfirmationDescription'),
+        props: {
+          variant: TYPOGRAPHY.H7,
+          align: 'center',
+          boxProps: {
+            margin: originIsMetaMask ? [0, 8, 4] : [0, 0, 4],
+          },
+        },
+      },
+      {
+        element: 'Typography',
+        key: 'only-add-networks-you-trust',
+        children: [
+          {
+            element: 'b',
+            key: 'bolded-text',
+            children: `${t('addEthereumChainConfirmationRisks')} `,
+          },
+          {
+            element: 'MetaMaskTranslation',
+            key: 'learn-about-risks',
+            props: {
+              translationKey: 'addEthereumChainConfirmationRisksLearnMore',
+              variables: [
+                {
+                  element: 'a',
+                  children: t('addEthereumChainConfirmationRisksLearnMoreLink'),
+                  key: 'addEthereumChainConfirmationRisksLearnMoreLink',
+                  props: {
+                    href:
+                      'https://metamask.zendesk.com/hc/en-us/articles/4404424659995',
+                    target: '__blank',
+                  },
+                },
+              ],
+            },
+          },
+        ],
+        props: {
+          variant: TYPOGRAPHY.H7,
+          align: 'center',
+          boxProps: {
+            margin: originIsMetaMask ? [0, 8] : 0,
+          },
+        },
+      },
+      {
+        element: 'TruncatedDefinitionList',
+        key: 'network-details',
+        props: {
+          title: t('networkDetails'),
+          tooltips: {
+            [t('networkName')]: t('networkNameDefinition'),
+            [t('networkURL')]: t('networkURLDefinition'),
+            [t('chainId')]: t('chainIdDefinition'),
+            [t('currencySymbol')]: t('currencySymbolDefinition'),
+            [t('blockExplorerUrl')]: t('blockExplorerUrlDefinition'),
+          },
+          dictionary: {
+            [t('networkName')]: pendingApproval.requestData.chainName,
+            [t('networkURL')]: pendingApproval.requestData.rpcUrl,
+            [t('chainId')]: parseInt(pendingApproval.requestData.chainId, 16),
+            [t('currencySymbol')]: pendingApproval.requestData.ticker,
+            [t('blockExplorerUrl')]: pendingApproval.requestData
+              .blockExplorerUrl,
+          },
+          prefaceKeys: [t('networkName'), t('networkURL'), t('chainId')],
+        },
+      },
+    ],
+    approvalText: t('approveButtonText'),
+    cancelText: t('cancel'),
+    onApprove: () =>
+      actions.resolvePendingApproval(
+        pendingApproval.id,
+        pendingApproval.requestData,
+      ),
+
+    onCancel: () =>
+      actions.rejectPendingApproval(
+        pendingApproval.id,
+        ethErrors.provider.userRejectedRequest(),
+      ),
+    networkDisplay: !originIsMetaMask,
+  };
+}
+
+const addEthereumChain = {
+  getAlerts,
+  getValues,
+};
+
+export default addEthereumChain;
---
+++ ui/pages/settings/networks-tab/networks-form/networks-form.js
@@ -0,0 +1,607 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import validUrl from 'valid-url';
+import log from 'loglevel';
+import classnames from 'classnames';
+import { useI18nContext } from '../../../../hooks/useI18nContext';
+import {
+  isPrefixedFormattedHexString,
+  isSafeChainId,
+} from '../../../../../shared/modules/network.utils';
+import { jsonRpcRequest } from '../../../../../shared/modules/rpc.utils';
+import ActionableMessage from '../../../../components/ui/actionable-message';
+import Popover from '../../../../components/ui/popover';
+import Button from '../../../../components/ui/button';
+import FormField from '../../../../components/ui/form-field';
+import { decimalToHex } from '../../../../helpers/utils/conversions.util';
+import {
+  setSelectedSettingsRpcUrl,
+  updateAndSetCustomRpc,
+  editRpc,
+  showModal,
+  setNewNetworkAdded,
+  addCustomNetworks,
+  cancelQRHardwareSignRequest,
+} from '../../../../store/actions';
+import ConfirmationPage from '../../../../pages/confirmation';
+import { getUnapprovedConfirmations } from '../../../../selectors';
+import {
+  DEFAULT_ROUTE,
+  NETWORKS_ROUTE,
+  CONFIRMATION_V_NEXT_ROUTE,
+} from '../../../../helpers/constants/routes';
+import fetchWithCache from '../../../../helpers/utils/fetch-with-cache';
+import { usePrevious } from '../../../../hooks/usePrevious';
+import AddNetwork from '../../../../components/app/add-network';
+import { MESSAGE_TYPE } from '../../../../../shared/constants/app';
+
+/**
+ * Attempts to convert the given chainId to a decimal string, for display
+ * purposes.
+ *
+ * Should be called with the props chainId whenever it is used to set the
+ * component's state.
+ *
+ * @param {unknown} chainId - The chainId to convert.
+ * @returns {string} The props chainId in decimal, or the original value if
+ * it can't be converted.
+ */
+const getDisplayChainId = (chainId) => {
+  if (!chainId || typeof chainId !== 'string' || !chainId.startsWith('0x')) {
+    return chainId;
+  }
+  return parseInt(chainId, 16).toString(10);
+};
+
+/**
+ * Prefixes a given id with '0x' if the prefix does not exist
+ *
+ * @param {string} chainId - The chainId to prefix
+ * @returns {string} The chainId, prefixed with '0x'
+ */
+const prefixChainId = (chainId) => {
+  let prefixedChainId = chainId;
+  if (!chainId.startsWith('0x')) {
+    prefixedChainId = `0x${parseInt(chainId, 10).toString(16)}`;
+  }
+  return prefixedChainId;
+};
+
+const isValidWhenAppended = (url) => {
+  const appendedRpc = `http://${url}`;
+  return validUrl.isWebUri(appendedRpc) && !url.match(/^https?:\/\/$/u);
+};
+
+const NetworksForm = ({
+  addNewNetwork,
+  isCurrentRpcTarget,
+  networksToRender,
+  selectedNetwork,
+}) => {
+  const t = useI18nContext();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { label, labelKey, viewOnly, rpcPrefs } = selectedNetwork;
+  const unapprovedConfirmations = useSelector(getUnapprovedConfirmations);
+  const selectedNetworkName = label || (labelKey && t(labelKey));
+  const [networkName, setNetworkName] = useState(selectedNetworkName || '');
+  const [showPopover, setShowPopover] = useState(false);
+  const [rpcUrl, setRpcUrl] = useState(selectedNetwork?.rpcUrl || '');
+  const [chainId, setChainId] = useState(selectedNetwork?.chainId || '');
+  const [ticker, setTicker] = useState(selectedNetwork?.ticker || '');
+  const [blockExplorerUrl, setBlockExplorerUrl] = useState(
+    selectedNetwork?.blockExplorerUrl || '',
+  );
+  const [errors, setErrors] = useState({});
+  const [warnings, setWarnings] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const resetForm = useCallback(() => {
+    setNetworkName(selectedNetworkName || '');
+    setRpcUrl(selectedNetwork.rpcUrl);
+    setChainId(getDisplayChainId(selectedNetwork.chainId));
+    setTicker(selectedNetwork?.ticker);
+    setBlockExplorerUrl(selectedNetwork?.blockExplorerUrl);
+    setErrors({});
+    setWarnings({});
+    setIsSubmitting(false);
+  }, [selectedNetwork, selectedNetworkName]);
+
+  const stateIsUnchanged = () => {
+    // These added conditions are in case the saved chainId is invalid, which
+    // was possible in versions <8.1 of the extension.
+    // Basically, we always want to be able to overwrite an invalid chain ID.
+    const chainIdIsUnchanged =
+      typeof selectedNetwork.chainId === 'string' &&
+      selectedNetwork.chainId.toLowerCase().startsWith('0x') &&
+      chainId === getDisplayChainId(selectedNetwork.chainId);
+    return (
+      rpcUrl === selectedNetwork.rpcUrl &&
+      chainIdIsUnchanged &&
+      ticker === selectedNetwork.ticker &&
+      networkName === selectedNetworkName &&
+      blockExplorerUrl === selectedNetwork.blockExplorerUrl
+    );
+  };
+
+  const prevAddNewNetwork = useRef();
+  const prevNetworkName = useRef();
+  const prevChainId = useRef();
+  const prevRpcUrl = useRef();
+  const prevTicker = useRef();
+  const prevBlockExplorerUrl = useRef();
+  useEffect(() => {
+    if (!prevAddNewNetwork.current && addNewNetwork) {
+      setNetworkName('');
+      setRpcUrl('');
+      setChainId('');
+      setTicker('');
+      setBlockExplorerUrl('');
+      setErrors({});
+      setIsSubmitting(false);
+    } else if (
+      prevNetworkName.current !== selectedNetworkName ||
+      prevRpcUrl.current !== selectedNetwork.rpcUrl ||
+      prevChainId.current !== selectedNetwork.chainId ||
+      prevTicker.current !== selectedNetwork.ticker ||
+      prevBlockExplorerUrl.current !== selectedNetwork.blockExplorerUrl
+    ) {
+      resetForm(selectedNetwork);
+    }
+  }, [
+    selectedNetwork,
+    selectedNetworkName,
+    addNewNetwork,
+    setNetworkName,
+    setRpcUrl,
+    setChainId,
+    setTicker,
+    setBlockExplorerUrl,
+    setErrors,
+    setIsSubmitting,
+    resetForm,
+  ]);
+
+  useEffect(() => {
+    return () => {
+      setNetworkName('');
+      setRpcUrl('');
+      setChainId('');
+      setTicker('');
+      setBlockExplorerUrl('');
+      setErrors({});
+      dispatch(setSelectedSettingsRpcUrl(''));
+    };
+  }, [
+    setNetworkName,
+    setRpcUrl,
+    setChainId,
+    setTicker,
+    setBlockExplorerUrl,
+    setErrors,
+    dispatch,
+  ]);
+
+  useEffect(() => {
+    const anAddNetworkConfirmationFromMetaMaskExists = unapprovedConfirmations.find(confirmation => {
+      return confirmation.origin === 'metamask' && confirmation.type === MESSAGE_TYPE.ADD_ETHEREUM_CHAIN;
+    })
+    if (!showPopover && anAddNetworkConfirmationFromMetaMaskExists) {
+      setShowPopover(true);
+    }
+  }, [unapprovedConfirmations, showPopover]);
+
+  const hasErrors = () => {
+    return Object.keys(errors).some((key) => {
+      const error = errors[key];
+      // Do not factor in duplicate chain id error for submission disabling
+      if (key === 'chainId' && error?.key === 'chainIdExistsErrorMsg') {
+        return false;
+      }
+      return error?.key && error?.msg;
+    });
+  };
+
+  const validateBlockExplorerURL = useCallback(
+    (url) => {
+      if (!validUrl.isWebUri(url) && url !== '') {
+        let errorKey;
+        let errorMessage;
+
+        if (isValidWhenAppended(url)) {
+          errorKey = 'urlErrorMsg';
+          errorMessage = t('urlErrorMsg');
+        } else {
+          errorKey = 'invalidBlockExplorerURL';
+          errorMessage = t('invalidBlockExplorerURL');
+        }
+
+        return {
+          key: errorKey,
+          msg: errorMessage,
+        };
+      }
+      return null;
+    },
+    [t],
+  );
+
+  const validateChainId = useCallback(
+    async (chainArg = '') => {
+      const formChainId = chainArg.trim();
+      let errorKey = '';
+      let errorMessage = '';
+      let radix = 10;
+      let hexChainId = formChainId;
+
+      if (!hexChainId.startsWith('0x')) {
+        try {
+          hexChainId = `0x${decimalToHex(hexChainId)}`;
+        } catch (err) {
+          return {
+            key: 'invalidHexNumber',
+            msg: t('invalidHexNumber'),
+          };
+        }
+      }
+
+      const [matchingChainId] = networksToRender.filter(
+        (e) => e.chainId === hexChainId && e.rpcUrl !== rpcUrl,
+      );
+
+      if (formChainId === '') {
+        return null;
+      } else if (matchingChainId) {
+        errorKey = 'chainIdExistsErrorMsg';
+        errorMessage = t('chainIdExistsErrorMsg', [
+          matchingChainId.label ?? matchingChainId.labelKey,
+        ]);
+      } else if (formChainId.startsWith('0x')) {
+        radix = 16;
+        if (!/^0x[0-9a-f]+$/iu.test(formChainId)) {
+          errorKey = 'invalidHexNumber';
+          errorMessage = t('invalidHexNumber');
+        } else if (!isPrefixedFormattedHexString(formChainId)) {
+          errorMessage = t('invalidHexNumberLeadingZeros');
+        }
+      } else if (!/^[0-9]+$/u.test(formChainId)) {
+        errorKey = 'invalidNumber';
+        errorMessage = t('invalidNumber');
+      } else if (formChainId.startsWith('0')) {
+        errorKey = 'invalidNumberLeadingZeros';
+        errorMessage = t('invalidNumberLeadingZeros');
+      } else if (!isSafeChainId(parseInt(formChainId, radix))) {
+        errorKey = 'invalidChainIdTooBig';
+        errorMessage = t('invalidChainIdTooBig');
+      }
+
+      let endpointChainId;
+      let providerError;
+
+      try {
+        endpointChainId = await jsonRpcRequest(rpcUrl, 'eth_chainId');
+      } catch (err) {
+        log.warn('Failed to fetch the chainId from the endpoint.', err);
+        providerError = err;
+      }
+
+      if (rpcUrl && formChainId) {
+        if (providerError || typeof endpointChainId !== 'string') {
+          errorKey = 'failedToFetchChainId';
+          errorMessage = t('failedToFetchChainId');
+        } else if (hexChainId !== endpointChainId) {
+          // Here, we are in an error state. The endpoint should always return a
+          // hexadecimal string. If the user entered a decimal string, we attempt
+          // to convert the endpoint's return value to decimal before rendering it
+          // in an error message in the form.
+          if (!formChainId.startsWith('0x')) {
+            try {
+              endpointChainId = parseInt(endpointChainId, 16).toString(10);
+            } catch (err) {
+              log.warn(
+                'Failed to convert endpoint chain ID to decimal',
+                endpointChainId,
+              );
+            }
+          }
+
+          errorKey = 'endpointReturnedDifferentChainId';
+          errorMessage = t('endpointReturnedDifferentChainId', [
+            endpointChainId.length <= 12
+              ? endpointChainId
+              : `${endpointChainId.slice(0, 9)}...`,
+          ]);
+        }
+      }
+      if (errorKey) {
+        return {
+          key: errorKey,
+          msg: errorMessage,
+        };
+      }
+
+      return null;
+    },
+    [rpcUrl, networksToRender, t],
+  );
+
+  /**
+   * Validates the ticker symbol by checking it against the nativeCurrency.symbol return
+   * value from chainid.network trusted chain data
+   * Assumes that all strings are non-empty and correctly formatted.
+   *
+   * @param {string} formChainId - The Chain ID currently entered in the form.
+   * @param {string} formTickerSymbol - The ticker/currency symbol currently entered in the form.
+   */
+  const validateTickerSymbol = useCallback(
+    async (formChainId, formTickerSymbol) => {
+      let warningKey;
+      let warningMessage;
+      let safeChainsList;
+      let providerError;
+
+      if (!formChainId || !formTickerSymbol) {
+        return null;
+      }
+
+      try {
+        safeChainsList = await fetchWithCache(
+          'https://chainid.network/chains.json',
+        );
+      } catch (err) {
+        log.warn('Failed to fetch the chainList from chainid.network', err);
+        providerError = err;
+      }
+
+      if (providerError || !Array.isArray(safeChainsList)) {
+        warningKey = 'failedToFetchTickerSymbolData';
+        warningMessage = t('failedToFetchTickerSymbolData');
+      } else {
+        const matchedChain = safeChainsList?.find(
+          (chain) => chain.chainId.toString() === formChainId,
+        );
+
+        if (matchedChain === undefined) {
+          warningKey = 'failedToFetchTickerSymbolData';
+          warningMessage = t('failedToFetchTickerSymbolData');
+        } else {
+          const returnedTickerSymbol = matchedChain.nativeCurrency?.symbol;
+          if (returnedTickerSymbol !== formTickerSymbol) {
+            warningKey = 'chainListReturnedDifferentTickerSymbol';
+            warningMessage = t('chainListReturnedDifferentTickerSymbol', [
+              formChainId,
+              returnedTickerSymbol,
+            ]);
+          }
+        }
+      }
+
+      if (warningKey) {
+        return {
+          key: warningKey,
+          msg: warningMessage,
+        };
+      }
+
+      return null;
+    },
+    [t],
+  );
+
+  const validateRPCUrl = useCallback(
+    (url) => {
+      const isValidUrl = validUrl.isWebUri(url);
+      const [
+        {
+          rpcUrl: matchingRPCUrl = null,
+          label: matchingRPCLabel,
+          labelKey: matchingRPCLabelKey,
+        } = {},
+      ] = networksToRender.filter((e) => e.rpcUrl === url);
+      const { rpcUrl: selectedNetworkRpcUrl } = selectedNetwork;
+
+      if (!isValidUrl && url !== '') {
+        let errorKey;
+        let errorMessage;
+        if (isValidWhenAppended(url)) {
+          errorKey = 'urlErrorMsg';
+          errorMessage = t('urlErrorMsg');
+        } else {
+          errorKey = 'invalidRPC';
+          errorMessage = t('invalidRPC');
+        }
+
+        return {
+          key: errorKey,
+          msg: errorMessage,
+        };
+      } else if (matchingRPCUrl && matchingRPCUrl !== selectedNetworkRpcUrl) {
+        return {
+          key: 'urlExistsErrorMsg',
+          msg: t('urlExistsErrorMsg', [
+            matchingRPCLabel ?? matchingRPCLabelKey,
+          ]),
+        };
+      }
+      return null;
+    },
+    [selectedNetwork, networksToRender, t],
+  );
+
+  // validation effect
+  const previousRpcUrl = usePrevious(rpcUrl);
+  const previousChainId = usePrevious(chainId);
+  const previousTicker = usePrevious(ticker);
+  const previousBlockExplorerUrl = usePrevious(blockExplorerUrl);
+  useEffect(() => {
+    if (viewOnly) {
+      return;
+    }
+
+    if (
+      previousRpcUrl === rpcUrl &&
+      previousChainId === chainId &&
+      previousTicker === ticker &&
+      previousBlockExplorerUrl === blockExplorerUrl
+    ) {
+      return;
+    }
+    async function validate() {
+      const chainIdError = await validateChainId(chainId);
+      const tickerWarning = await validateTickerSymbol(chainId, ticker);
+      const blockExplorerError = validateBlockExplorerURL(blockExplorerUrl);
+      const rpcUrlError = validateRPCUrl(rpcUrl);
+      setErrors({
+        ...errors,
+        chainId: chainIdError,
+        blockExplorerUrl: blockExplorerError,
+        rpcUrl: rpcUrlError,
+      });
+      setWarnings({
+        ...warnings,
+        ticker: tickerWarning,
+      });
+    }
+
+    validate();
+  }, [
+    errors,
+    warnings,
+    rpcUrl,
+    chainId,
+    ticker,
+    blockExplorerUrl,
+    viewOnly,
+    label,
+    previousRpcUrl,
+    previousChainId,
+    previousTicker,
+    previousBlockExplorerUrl,
+    validateBlockExplorerURL,
+    validateChainId,
+    validateTickerSymbol,
+    validateRPCUrl,
+  ]);
+
+  const onSubmit = async () => {
+    setIsSubmitting(true);
+    try {
+      const formChainId = chainId.trim().toLowerCase();
+      const prefixedChainId = prefixChainId(formChainId);
+
+      // After this point, isSubmitting will be reset in componentDidUpdate
+      if (selectedNetwork.rpcUrl && rpcUrl !== selectedNetwork.rpcUrl) {
+        await dispatch(
+          editRpc(
+            selectedNetwork.rpcUrl,
+            rpcUrl,
+            prefixedChainId,
+            ticker,
+            networkName,
+            {
+              ...rpcPrefs,
+              blockExplorerUrl: blockExplorerUrl || rpcPrefs?.blockExplorerUrl,
+            },
+          ),
+        );
+      } else {
+        await dispatch(
+          updateAndSetCustomRpc(rpcUrl, prefixedChainId, ticker, networkName, {
+            ...rpcPrefs,
+            blockExplorerUrl: blockExplorerUrl || rpcPrefs?.blockExplorerUrl,
+          }),
+        );
+      }
+
+      if (addNewNetwork) {
+        dispatch(setNewNetworkAdded(networkName));
+        history.push(DEFAULT_ROUTE);
+      }
+    } catch (error) {
+      setIsSubmitting(false);
+      throw error;
+    }
+  };
+
+  const onCancel = () => {
+    if (addNewNetwork) {
+      dispatch(setSelectedSettingsRpcUrl(''));
+      history.push(NETWORKS_ROUTE);
+    } else {
+      resetForm();
+    }
+  };
+
+  const onDelete = () => {
+    dispatch(
+      showModal({
+        name: 'CONFIRM_DELETE_NETWORK',
+        target: selectedNetwork.rpcUrl,
+        onConfirm: () => {
+          resetForm();
+          dispatch(setSelectedSettingsRpcUrl(''));
+          history.push(NETWORKS_ROUTE);
+        },
+      }),
+    );
+  };
+  const deletable = !isCurrentRpcTarget && !viewOnly && !addNewNetwork;
+  const stateUnchanged = stateIsUnchanged();
+  const isSubmitDisabled =
+    hasErrors() ||
+    isSubmitting ||
+    stateUnchanged ||
+    !rpcUrl ||
+    !chainId ||
+    !ticker;
+
+  const FEATURED_RPCS = [
+    {
+      chainId: '0x66',
+      nickname: 'Arbitrum Testnet',
+      rpcUrl:
+        'https://arbitrum-rinkeby.infura.io/v3/2b6d4a83d89a438eb1b5d036788ab29c',
+      ticker: 'ARETH',
+      rpcPrefs: {
+        blockExplorerUrl: 'https://testnet.arbiscan.io/',
+        imageUrl: './images/optimism.svg',
+      },
+    },
+  ];
+
+  return (
+    <>
+      <div
+        className={classnames({
+          'networks-tab__network-form': !addNewNetwork,
+          'networks-tab__add-network-form': addNewNetwork,
+        })}
+      >
+        <AddNetwork
+          featuredRPCS={FEATURED_RPCS}
+          onAddNetworkClick={async () => {
+            dispatch(addCustomNetworks());
+          }}
+        />
+      </div>
+      {showPopover && <Popover>
+        <ConfirmationPage />
+      </Popover>}
+    </>
+  );
+};
+
+NetworksForm.propTypes = {
+  addNewNetwork: PropTypes.bool,
+  isCurrentRpcTarget: PropTypes.bool,
+  networksToRender: PropTypes.array.isRequired,
+  selectedNetwork: PropTypes.object,
+};
+
+NetworksForm.defaultProps = {
+  selectedNetwork: {},
+};
+
+export default NetworksForm;
