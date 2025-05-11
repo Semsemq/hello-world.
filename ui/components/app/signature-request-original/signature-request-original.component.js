3848b035e80d9aee9e2aab3d2292552396098ede
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:01:26 +0300

    Initialize new-onboarding-privacy

---
+++ ui/components/app/signature-request-original/signature-request-original.component.js
@@ -0,0 +1,373 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { ObjectInspector } from 'react-inspector';
+import LedgerInstructionField from '../ledger-instruction-field';
+
+import { MESSAGE_TYPE } from '../../../../shared/constants/app';
+import { EVENT } from '../../../../shared/constants/metametrics';
+import { getURLHostName } from '../../../helpers/utils/util';
+import { conversionUtil } from '../../../../shared/modules/conversion.utils';
+import { stripHexPrefix } from '../../../../shared/modules/hexstring-utils';
+import Button from '../../ui/button';
+import SiteOrigin from '../../ui/site-origin';
+import NetworkAccountBalanceHeader from '../network-account-balance-header';
+import Typography from '../../ui/typography/typography';
+import {
+  TYPOGRAPHY,
+  FONT_WEIGHT,
+  COLORS,
+  TEXT_ALIGN,
+} from '../../../helpers/constants/design-system';
+import { NETWORK_TYPES } from '../../../../shared/constants/network';
+import SignatureRequestOriginalWarning from './signature-request-original-warning';
+
+export default class SignatureRequestOriginal extends Component {
+  static contextTypes = {
+    t: PropTypes.func.isRequired,
+    trackEvent: PropTypes.func.isRequired,
+  };
+
+  static propTypes = {
+    fromAccount: PropTypes.shape({
+      address: PropTypes.string.isRequired,
+      balance: PropTypes.string,
+      name: PropTypes.string,
+    }).isRequired,
+    cancel: PropTypes.func.isRequired,
+    clearConfirmTransaction: PropTypes.func.isRequired,
+    conversionRate: PropTypes.number,
+    history: PropTypes.object.isRequired,
+    mostRecentOverviewPage: PropTypes.string.isRequired,
+    sign: PropTypes.func.isRequired,
+    txData: PropTypes.object.isRequired,
+    subjectMetadata: PropTypes.object,
+    hardwareWalletRequiresConnection: PropTypes.bool,
+    isLedgerWallet: PropTypes.bool,
+    nativeCurrency: PropTypes.string.isRequired,
+    messagesCount: PropTypes.number,
+    showRejectTransactionsConfirmationModal: PropTypes.func.isRequired,
+    cancelAll: PropTypes.func.isRequired,
+    provider: PropTypes.object,
+  };
+
+  state = {
+    fromAccount: this.props.fromAccount,
+    showSignatureRequestWarning: false,
+  };
+
+  getNetworkName() {
+    const { provider } = this.props;
+    const providerName = provider.type;
+    const { t } = this.context;
+
+    switch (providerName) {
+      case NETWORK_TYPES.MAINNET:
+        return t('mainnet');
+      case NETWORK_TYPES.GOERLI:
+        return t('goerli');
+      case NETWORK_TYPES.SEPOLIA:
+        return t('sepolia');
+      case NETWORK_TYPES.LOCALHOST:
+        return t('localhost');
+      default:
+        return provider.nickname || t('unknownNetwork');
+    }
+  }
+
+  msgHexToText = (hex) => {
+    try {
+      const stripped = stripHexPrefix(hex);
+      const buff = Buffer.from(stripped, 'hex');
+      return buff.length === 32 ? hex : buff.toString('utf8');
+    } catch (e) {
+      return hex;
+    }
+  };
+
+  renderTypedData = (data) => {
+    const { t } = this.context;
+    const { domain, message } = JSON.parse(data);
+    return (
+      <div className="request-signature__typed-container">
+        {domain ? (
+          <div>
+            <h1>{t('domain')}</h1>
+            <ObjectInspector data={domain} expandLevel={1} name="domain" />
+          </div>
+        ) : (
+          ''
+        )}
+        {message ? (
+          <div>
+            <h1>{t('message')}</h1>
+            <ObjectInspector data={message} expandLevel={1} name="message" />
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    );
+  };
+
+  renderBody = () => {
+    let rows;
+    const notice = `${this.context.t('youSign')}:`;
+
+    const { txData, subjectMetadata } = this.props;
+    const {
+      type,
+      msgParams: { data },
+    } = txData;
+
+    if (type === MESSAGE_TYPE.PERSONAL_SIGN) {
+      rows = [
+        { name: this.context.t('message'), value: this.msgHexToText(data) },
+      ];
+    } else if (type === MESSAGE_TYPE.ETH_SIGN_TYPED_DATA) {
+      rows = data;
+    } else if (type === MESSAGE_TYPE.ETH_SIGN) {
+      rows = [{ name: this.context.t('message'), value: data }];
+    }
+
+    const targetSubjectMetadata = txData.msgParams.origin
+      ? subjectMetadata?.[txData.msgParams.origin]
+      : null;
+
+    return (
+      <div className="request-signature__body">
+        <div className="request-signature__origin">
+          <SiteOrigin
+            siteOrigin={txData.msgParams.origin}
+            iconSrc={targetSubjectMetadata?.iconUrl}
+            iconName={
+              getURLHostName(targetSubjectMetadata?.origin) ||
+              targetSubjectMetadata?.origin
+            }
+            chip
+          />
+        </div>
+
+        <Typography
+          className="request-signature__content__title"
+          variant={TYPOGRAPHY.H3}
+          fontWeight={FONT_WEIGHT.BOLD}
+        >
+          {this.context.t('sigRequest')}
+        </Typography>
+        <Typography
+          className="request-signature__content__subtitle"
+          variant={TYPOGRAPHY.H7}
+          color={COLORS.TEXT_ALTERNATIVE}
+          align={TEXT_ALIGN.CENTER}
+          margin={12}
+          marginTop={3}
+        >
+          {this.context.t('signatureRequestGuidance')}
+        </Typography>
+
+        <div className={classnames('request-signature__notice')}>{notice}</div>
+        <div className="request-signature__rows">
+          {rows.map(({ name, value }, index) => {
+            if (typeof value === 'boolean') {
+              // eslint-disable-next-line no-param-reassign
+              value = value.toString();
+            }
+            return (
+              <div
+                className="request-signature__row"
+                key={`request-signature-row-${index}`}
+              >
+                <div className="request-signature__row-title">{`${name}:`}</div>
+                <div className="request-signature__row-value">{value}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  onSubmit = async (event) => {
+    const { clearConfirmTransaction, history, mostRecentOverviewPage, sign } =
+      this.props;
+    const { trackEvent, type } = this.context;
+
+    await sign(event);
+    trackEvent({
+      category: EVENT.CATEGORIES.TRANSACTIONS,
+      event: 'Confirm',
+      properties: {
+        action: 'Sign Request',
+        legacy_event: true,
+        type,
+      },
+    });
+    clearConfirmTransaction();
+    history.push(mostRecentOverviewPage);
+  };
+
+  onCancel = async (event) => {
+    const { clearConfirmTransaction, history, mostRecentOverviewPage, cancel } =
+      this.props;
+    const { trackEvent, type } = this.context;
+
+    await cancel(event);
+    trackEvent({
+      category: EVENT.CATEGORIES.TRANSACTIONS,
+      event: 'Cancel',
+      properties: {
+        action: 'Sign Request',
+        legacy_event: true,
+        type,
+      },
+    });
+    clearConfirmTransaction();
+    history.push(mostRecentOverviewPage);
+  };
+
+  renderFooter = () => {
+    const {
+      cancel,
+      sign,
+      clearConfirmTransaction,
+      history,
+      mostRecentOverviewPage,
+      txData: { type },
+      hardwareWalletRequiresConnection,
+    } = this.props;
+    const { trackEvent, t } = this.context;
+
+    return (
+      <div className="request-signature__footer">
+        <Button
+          type="secondary"
+          large
+          className="request-signature__footer__cancel-button"
+          onClick={async (event) => {
+            await cancel(event);
+            trackEvent({
+              category: EVENT.CATEGORIES.TRANSACTIONS,
+              event: 'Cancel',
+              properties: {
+                action: 'Sign Request',
+                legacy_event: true,
+                type,
+              },
+            });
+            clearConfirmTransaction();
+            history.push(mostRecentOverviewPage);
+          }}
+        >
+          {t('reject')}
+        </Button>
+        <Button
+          data-testid="request-signature__sign"
+          type="primary"
+          large
+          className="request-signature__footer__sign-button"
+          disabled={hardwareWalletRequiresConnection}
+          onClick={async (event) => {
+            if (type === MESSAGE_TYPE.ETH_SIGN) {
+              this.setState({ showSignatureRequestWarning: true });
+            } else {
+              await sign(event);
+              trackEvent({
+                category: EVENT.CATEGORIES.TRANSACTIONS,
+                event: 'Confirm',
+                properties: {
+                  action: 'Sign Request',
+                  legacy_event: true,
+                  type,
+                },
+              });
+              clearConfirmTransaction();
+              history.push(mostRecentOverviewPage);
+            }
+          }}
+        >
+          {t('sign')}
+        </Button>
+      </div>
+    );
+  };
+
+  handleCancelAll = () => {
+    const {
+      cancelAll,
+      clearConfirmTransaction,
+      history,
+      mostRecentOverviewPage,
+      showRejectTransactionsConfirmationModal,
+      messagesCount,
+    } = this.props;
+    const unapprovedTxCount = messagesCount;
+
+    showRejectTransactionsConfirmationModal({
+      unapprovedTxCount,
+      onSubmit: async () => {
+        await cancelAll();
+        clearConfirmTransaction();
+        history.push(mostRecentOverviewPage);
+      },
+    });
+  };
+
+  render = () => {
+    const { messagesCount, conversionRate, nativeCurrency } = this.props;
+    const {
+      fromAccount: { address, balance, name },
+      showSignatureRequestWarning,
+    } = this.state;
+    const { t } = this.context;
+
+    const rejectNText = t('rejectRequestsN', [messagesCount]);
+    const currentNetwork = this.getNetworkName();
+
+    const balanceInBaseAsset = conversionUtil(balance, {
+      fromNumericBase: 'hex',
+      toNumericBase: 'dec',
+      fromDenomination: 'WEI',
+      numberOfDecimals: 6,
+      conversionRate,
+    });
+
+    return (
+      <div className="request-signature__container">
+        <div className="request-signature__account">
+          <NetworkAccountBalanceHeader
+            networkName={currentNetwork}
+            accountName={name}
+            accountBalance={balanceInBaseAsset}
+            tokenName={nativeCurrency}
+            accountAddress={address}
+          />
+        </div>
+        {this.renderBody()}
+        {this.props.isLedgerWallet ? (
+          <div className="confirm-approve-content__ledger-instruction-wrapper">
+            <LedgerInstructionField showDataInstruction />
+          </div>
+        ) : null}
+        {showSignatureRequestWarning && (
+          <SignatureRequestOriginalWarning
+            senderAddress={address}
+            name={name}
+            onSubmit={async (event) => await this.onSubmit(event)}
+            onCancel={async (event) => await this.onCancel(event)}
+          />
+        )}
+        {this.renderFooter()}
+        {messagesCount > 1 ? (
+          <Button
+            type="link"
+            className="request-signature__container__reject"
+            onClick={() => this.handleCancelAll()}
+          >
+            {rejectNText}
+          </Button>
+        ) : null}
+      </div>
+    );
+  };
+}
---
+++ ui/pages/onboarding-flow/metametrics/metametrics.js
@@ -0,0 +1,239 @@
+import React, { useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Typography from '../../../components/ui/typography/typography';
+import {
+  TYPOGRAPHY,
+  FONT_WEIGHT,
+  TEXT_ALIGN,
+  COLORS,
+} from '../../../helpers/constants/design-system';
+import Button from '../../../components/ui/button';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+import { setParticipateInMetaMetrics } from '../../../store/actions';
+import {
+  getFirstTimeFlowTypeRoute,
+  getFirstTimeFlowType,
+  getParticipateInMetaMetrics,
+} from '../../../selectors';
+
+import { EVENT, EVENT_NAMES } from '../../../../shared/constants/metametrics';
+
+import { MetaMetricsContext } from '../../../contexts/metametrics';
+import { Icon } from '../../../components/component-library';
+
+export default function OnboardingMetametrics() {
+  const t = useI18nContext();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const nextRoute = useSelector(getFirstTimeFlowTypeRoute);
+  const firstTimeFlowType = useSelector(getFirstTimeFlowType);
+
+  const participateInMetaMetrics = useSelector(getParticipateInMetaMetrics);
+
+  const trackEvent = useContext(MetaMetricsContext);
+
+  const onConfirm = async () => {
+    const [, metaMetricsId] = await dispatch(setParticipateInMetaMetrics(true));
+
+    const isInitiallyNotParticipating = !participateInMetaMetrics;
+
+    try {
+      if (isInitiallyNotParticipating) {
+        trackEvent(
+          {
+            category: EVENT.CATEGORIES.ONBOARDING,
+            event: EVENT_NAMES.METRICS_OPT_IN,
+            properties: {
+              action: 'Metrics Option',
+              legacy_event: true,
+            },
+          },
+          {
+            isOptIn: true,
+            flushImmediately: true,
+          },
+        );
+      }
+      trackEvent(
+        {
+          category: EVENT.CATEGORIES.ONBOARDING,
+          event: EVENT_NAMES.WALLET_SETUP_STARTED,
+          properties: {
+            account_type:
+              firstTimeFlowType === 'create'
+                ? EVENT.ACCOUNT_TYPES.DEFAULT
+                : EVENT.ACCOUNT_TYPES.IMPORTED,
+          },
+        },
+        {
+          isOptIn: true,
+          metaMetricsId,
+          flushImmediately: true,
+        },
+      );
+    } finally {
+      history.push(nextRoute);
+    }
+  };
+
+  const onCancel = async () => {
+    await dispatch(setParticipateInMetaMetrics(false));
+
+    const isInitiallyParticipatingOrNotSet =
+      participateInMetaMetrics === null || participateInMetaMetrics;
+
+    try {
+      if (isInitiallyParticipatingOrNotSet) {
+        trackEvent(
+          {
+            category: EVENT.CATEGORIES.ONBOARDING,
+            event: EVENT_NAMES.METRICS_OPT_OUT,
+            properties: {
+              action: 'Metrics Option',
+              legacy_event: true,
+            },
+          },
+          {
+            isOptIn: true,
+            flushImmediately: true,
+          },
+        );
+      }
+    } finally {
+      history.push(nextRoute);
+    }
+  };
+
+  return (
+    <div
+      className="onboarding-metametrics"
+      data-testid="onboarding-metametrics"
+    >
+      <Typography
+        variant={TYPOGRAPHY.H2}
+        align={TEXT_ALIGN.CENTER}
+        fontWeight={FONT_WEIGHT.BOLD}
+      >
+        {t('metametricsHelpImproveMetaMask')}
+      </Typography>
+      <Typography align={TEXT_ALIGN.CENTER}>
+        {t('onboardingMetametricsIntro1', [
+          <Typography
+            variant={TYPOGRAPHY.Span}
+            fontWeight={FONT_WEIGHT.BOLD}
+            color={COLORS.TEXT_ALTERNATIVE}
+            key="emphasized-text"
+          >
+            {t('onboardingMetametricsPrivateEmphasizedText')}
+          </Typography>,
+        ])}
+      </Typography>
+      <Typography align={TEXT_ALIGN.CENTER} marginBottom={8}>
+        {t('onboardingMetametricsIntro2')}
+      </Typography>
+
+      <div className="onboarding-metametrics__grouping">
+        <Icon
+          name="eye-slash-filled"
+          color={COLORS.PRIMARY_DEFAULT}
+          marginRight={3}
+        />
+        <div className="onboarding-metametrics__grouping-text">
+          <Typography
+            variant={TYPOGRAPHY.H4}
+            fontWeight={FONT_WEIGHT.BOLD}
+            marginTop={0}
+          >
+            {t('onboardingMetametricsAnonymousTitle')}
+          </Typography>
+          <Typography color={COLORS.TEXT_ALTERNATIVE} marginBottom={8}>
+            {t('onboardingMetametricsAnonymousText')}
+          </Typography>
+        </div>
+      </div>
+
+      <div className="onboarding-metametrics__grouping">
+        <Icon
+          name="security-key-filled"
+          color={COLORS.PRIMARY_DEFAULT}
+          marginRight={3}
+        />
+        <div className="onboarding-metametrics__grouping-text">
+          <Typography
+            variant={TYPOGRAPHY.H4}
+            fontWeight={FONT_WEIGHT.BOLD}
+            marginTop={0}
+          >
+            {t('onboardingMetametricsPrivateTitle')}
+          </Typography>
+          <Typography color={COLORS.TEXT_ALTERNATIVE} marginBottom={8}>
+            {t('onboardingMetametricsPrivateText', [
+              <a
+                href="https://consensys.net/privacy-policy/"
+                target="_blank"
+                rel="noopener noreferrer"
+                key="infura-link"
+              >
+                {t('infura')}
+              </a>,
+            ])}
+          </Typography>
+        </div>
+      </div>
+
+      <div className="onboarding-metametrics__grouping">
+        <Icon
+          name="setting-filled"
+          color={COLORS.PRIMARY_DEFAULT}
+          marginRight={3}
+        />
+        <div className="onboarding-metametrics__grouping-text">
+          <Typography
+            variant={TYPOGRAPHY.H4}
+            fontWeight={FONT_WEIGHT.BOLD}
+            marginTop={0}
+          >
+            {t('onboardingMetametricsOptionalTitle')}
+          </Typography>
+          <Typography marginBottom={8} color={COLORS.TEXT_ALTERNATIVE}>
+            {t('onboardingMetametricsOptionalText')}
+          </Typography>
+        </div>
+      </div>
+
+      <div className="onboarding-metametrics__grouping">
+        <Typography color={COLORS.TEXT_ALTERNATIVE} align={TEXT_ALIGN.CENTER}>
+          {t('onboardingMetametricsFooter', [
+            <a
+              href="https://consensys.net/privacy-policy/"
+              key="link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {t('gdprMessagePrivacyPolicy')}
+            </a>,
+          ])}
+        </Typography>
+      </div>
+
+      <div className="onboarding-metametrics__buttons">
+        <Button
+          data-testid="metametrics-no-thanks"
+          type="secondary"
+          onClick={onCancel}
+        >
+          {t('onboardingMetametricsButtonDontShare')}
+        </Button>
+        <Button
+          data-testid="metametrics-i-agree"
+          type="primary"
+          onClick={onConfirm}
+        >
+          {t('onboardingMetametricsButtonShare')}
+        </Button>
+      </div>
+    </div>
+  );
+}
