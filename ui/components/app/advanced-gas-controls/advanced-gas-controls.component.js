7b306eb7a81453e4f409f982dd23e6d2d624e7cf
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 22:56:50 +0300

    Initialize send-flow-eip-1559-plus-polyfill

---
+++ ui/components/app/advanced-gas-controls/advanced-gas-controls.component.js
@@ -0,0 +1,237 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+
+import { I18nContext } from '../../../contexts/i18n';
+import Typography from '../../ui/typography/typography';
+import {
+  FONT_WEIGHT,
+  TYPOGRAPHY,
+  COLORS,
+} from '../../../helpers/constants/design-system';
+import FormField from '../../ui/form-field';
+import {
+  GAS_ESTIMATE_TYPES,
+  GAS_RECOMMENDATIONS,
+} from '../../../../shared/constants/gas';
+import { getGasFormErrorText } from '../../../helpers/constants/gas';
+
+const DEFAULT_ESTIMATES_LEVEL = 'medium';
+
+export default function AdvancedGasControls({
+  estimateToUse,
+  gasFeeEstimates,
+  gasEstimateType,
+  maxPriorityFee,
+  maxFee,
+  setMaxPriorityFee,
+  setMaxFee,
+  onManualChange,
+  gasLimit,
+  setGasLimit,
+  gasPrice,
+  setGasPrice,
+  maxPriorityFeeFiat,
+  maxFeeFiat,
+  gasErrors,
+  networkSupportsEIP1559,
+}) {
+  const t = useContext(I18nContext);
+
+  const suggestedValues = {};
+
+  if (networkSupportsEIP1559) {
+    suggestedValues.maxFeePerGas =
+      gasFeeEstimates?.[estimateToUse]?.suggestedMaxFeePerGas ||
+      gasFeeEstimates?.gasPrice;
+    suggestedValues.maxPriorityFeePerGas =
+      gasFeeEstimates?.[estimateToUse]?.suggestedMaxPriorityFeePerGas ||
+      suggestedValues.maxFeePerGas;
+  } else {
+    switch (gasEstimateType) {
+      case GAS_ESTIMATE_TYPES.LEGACY:
+        suggestedValues.gasPrice = gasFeeEstimates?.[estimateToUse];
+        break;
+      case GAS_ESTIMATE_TYPES.ETH_GASPRICE:
+        suggestedValues.gasPrice = gasFeeEstimates?.gasPrice;
+        break;
+      default:
+        break;
+    }
+  }
+
+  const showFeeMarketFields = networkSupportsEIP1559;
+
+  return (
+    <div className="advanced-gas-controls">
+      <FormField
+        titleText={t('gasLimit')}
+        error={
+          gasErrors?.gasLimit
+            ? getGasFormErrorText(gasErrors.gasLimit, t)
+            : null
+        }
+        onChange={setGasLimit}
+        tooltipText={t('editGasLimitTooltip')}
+        value={gasLimit}
+        allowDecimals={false}
+        numeric
+        autoFocus
+      />
+      {showFeeMarketFields ? (
+        <>
+          <FormField
+            titleText={t('maxPriorityFee')}
+            titleUnit="(GWEI)"
+            tooltipText={t('editGasMaxPriorityFeeTooltip')}
+            onChange={(value) => {
+              onManualChange?.();
+              setMaxPriorityFee(value);
+            }}
+            value={maxPriorityFee}
+            detailText={maxPriorityFeeFiat}
+            numeric
+            titleDetail={
+              suggestedValues.maxPriorityFeePerGas && (
+                <>
+                  <Typography
+                    tag="span"
+                    color={COLORS.UI4}
+                    variant={TYPOGRAPHY.H8}
+                    fontWeight={FONT_WEIGHT.BOLD}
+                  >
+                    {t('gasFeeEstimate')}:
+                  </Typography>{' '}
+                  <Typography
+                    tag="span"
+                    color={COLORS.UI4}
+                    variant={TYPOGRAPHY.H8}
+                  >
+                    {
+                      gasFeeEstimates?.[DEFAULT_ESTIMATES_LEVEL]
+                        ?.suggestedMaxPriorityFeePerGas
+                    }
+                  </Typography>
+                </>
+              )
+            }
+            error={
+              gasErrors?.maxPriorityFee
+                ? getGasFormErrorText(gasErrors.maxPriorityFee, t)
+                : null
+            }
+          />
+          <FormField
+            titleText={t('maxFee')}
+            titleUnit="(GWEI)"
+            tooltipText={t('editGasMaxFeeTooltip')}
+            onChange={(value) => {
+              onManualChange?.();
+              setMaxFee(value);
+            }}
+            value={maxFee}
+            numeric
+            detailText={maxFeeFiat}
+            titleDetail={
+              suggestedValues.maxFeePerGas && (
+                <>
+                  <Typography
+                    tag="span"
+                    color={COLORS.UI4}
+                    variant={TYPOGRAPHY.H8}
+                    fontWeight={FONT_WEIGHT.BOLD}
+                  >
+                    {t('gasFeeEstimate')}:
+                  </Typography>{' '}
+                  <Typography
+                    tag="span"
+                    color={COLORS.UI4}
+                    variant={TYPOGRAPHY.H8}
+                  >
+                    {
+                      gasFeeEstimates?.[DEFAULT_ESTIMATES_LEVEL]
+                        ?.suggestedMaxFeePerGas
+                    }
+                  </Typography>
+                </>
+              )
+            }
+            error={
+              gasErrors?.maxFee
+                ? getGasFormErrorText(gasErrors.maxFee, t)
+                : null
+            }
+          />
+        </>
+      ) : (
+        <>
+          <FormField
+            titleText={t('advancedGasPriceTitle')}
+            titleUnit="(GWEI)"
+            onChange={(value) => {
+              onManualChange?.();
+              setGasPrice(value);
+            }}
+            tooltipText={t('editGasPriceTooltip')}
+            value={gasPrice}
+            numeric
+            titleDetail={
+              suggestedValues.gasPrice && (
+                <>
+                  <Typography
+                    tag="span"
+                    color={COLORS.UI4}
+                    variant={TYPOGRAPHY.H8}
+                    fontWeight={FONT_WEIGHT.BOLD}
+                  >
+                    {t('gasFeeEstimate')}:
+                  </Typography>{' '}
+                  <Typography
+                    tag="span"
+                    color={COLORS.UI4}
+                    variant={TYPOGRAPHY.H8}
+                  >
+                    {suggestedValues.gasPrice}
+                  </Typography>
+                </>
+              )
+            }
+          />
+        </>
+      )}
+    </div>
+  );
+}
+
+AdvancedGasControls.propTypes = {
+  estimateToUse: PropTypes.oneOf(Object.values(GAS_RECOMMENDATIONS)),
+  gasFeeEstimates: PropTypes.oneOf([
+    PropTypes.shape({
+      gasPrice: PropTypes.string,
+    }),
+    PropTypes.shape({
+      low: PropTypes.string,
+      medium: PropTypes.string,
+      high: PropTypes.string,
+    }),
+    PropTypes.shape({
+      low: PropTypes.object,
+      medium: PropTypes.object,
+      high: PropTypes.object,
+      estimatedBaseFee: PropTypes.string,
+    }),
+  ]),
+  gasEstimateType: PropTypes.oneOf(Object.values(GAS_ESTIMATE_TYPES)),
+  setMaxPriorityFee: PropTypes.func,
+  setMaxFee: PropTypes.func,
+  maxPriorityFee: PropTypes.number,
+  maxFee: PropTypes.number,
+  onManualChange: PropTypes.func,
+  gasLimit: PropTypes.number,
+  setGasLimit: PropTypes.func,
+  gasPrice: PropTypes.number,
+  setGasPrice: PropTypes.func,
+  maxPriorityFeeFiat: PropTypes.string,
+  maxFeeFiat: PropTypes.string,
+  gasErrors: PropTypes.object,
+  networkSupportsEIP1559: PropTypes.object,
+};
---
+++ ui/components/app/edit-gas-popover/edit-gas-popover.component.js
@@ -0,0 +1,246 @@
+import React, { useCallback, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { isEIP1559Network } from '../../../ducks/metamask/metamask';
+import { useGasFeeInputs } from '../../../hooks/useGasFeeInputs';
+import { useShouldAnimateGasEstimations } from '../../../hooks/useShouldAnimateGasEstimations';
+
+import { EDIT_GAS_MODES } from '../../../../shared/constants/gas';
+
+import {
+  decGWEIToHexWEI,
+  decimalToHex,
+} from '../../../helpers/utils/conversions.util';
+
+import Popover from '../../ui/popover';
+import Button from '../../ui/button';
+import EditGasDisplay from '../edit-gas-display';
+import EditGasDisplayEducation from '../edit-gas-display-education';
+
+import { I18nContext } from '../../../contexts/i18n';
+import {
+  createCancelTransaction,
+  createSpeedUpTransaction,
+  hideModal,
+  hideSidebar,
+  updateTransaction,
+} from '../../../store/actions';
+import LoadingHeartBeat from '../../ui/loading-heartbeat';
+
+export default function EditGasPopover({
+  popoverTitle = '',
+  confirmButtonText = '',
+  editGasDisplayProps = {},
+  defaultEstimateToUse = 'medium',
+  transaction,
+  mode,
+  onClose,
+}) {
+  const t = useContext(I18nContext);
+  const dispatch = useDispatch();
+  const showSidebar = useSelector((state) => state.appState.sidebar.isOpen);
+  const networkSupports1559 = useSelector(isEIP1559Network);
+
+  const shouldAnimate = useShouldAnimateGasEstimations();
+
+  const showEducationButton =
+    mode === EDIT_GAS_MODES.MODIFY_IN_PLACE && process.env.SHOW_EIP_1559_UI;
+  const [showEducationContent, setShowEducationContent] = useState(false);
+
+  const [warning] = useState(null);
+
+  const [
+    dappSuggestedGasFeeAcknowledged,
+    setDappSuggestedGasFeeAcknowledged,
+  ] = useState(false);
+
+  const {
+    maxPriorityFeePerGas,
+    setMaxPriorityFeePerGas,
+    maxPriorityFeePerGasFiat,
+    maxFeePerGas,
+    setMaxFeePerGas,
+    maxFeePerGasFiat,
+    estimatedMaximumNative,
+    isGasEstimatesLoading,
+    gasFeeEstimates,
+    gasEstimateType,
+    gasPrice,
+    setGasPrice,
+    gasLimit,
+    setGasLimit,
+    estimateToUse,
+    setEstimateToUse,
+    estimatedMinimumFiat,
+    estimatedMaximumFiat,
+    hasGasErrors,
+    gasErrors,
+    onManualChange,
+  } = useGasFeeInputs(defaultEstimateToUse, transaction);
+
+  const [showAdvancedForm, setShowAdvancedForm] = useState(
+    !estimateToUse || hasGasErrors,
+  );
+
+  /**
+   * Temporary placeholder, this should be managed by the parent component but
+   * we will be extracting this component from the hard to maintain modal/
+   * sidebar component. For now this is just to be able to appropriately close
+   * the modal in testing
+   */
+  const closePopover = useCallback(() => {
+    if (onClose) {
+      onClose();
+    } else if (showSidebar) {
+      dispatch(hideSidebar());
+    } else {
+      dispatch(hideModal());
+    }
+  }, [showSidebar, onClose, dispatch]);
+
+  const onSubmit = useCallback(() => {
+    if (!transaction || !mode) {
+      closePopover();
+    }
+
+    const newGasSettings = networkSupports1559
+      ? {
+          gas: decimalToHex(gasLimit),
+          gasLimit: decimalToHex(gasLimit),
+          maxFeePerGas: decGWEIToHexWEI(maxFeePerGas ?? gasPrice),
+          maxPriorityFeePerGas: decGWEIToHexWEI(
+            maxPriorityFeePerGas ?? maxFeePerGas ?? gasPrice,
+          ),
+        }
+      : {
+          gas: decimalToHex(gasLimit),
+          gasLimit: decimalToHex(gasLimit),
+          gasPrice: decGWEIToHexWEI(gasPrice),
+        };
+
+    switch (mode) {
+      case EDIT_GAS_MODES.CANCEL:
+        dispatch(createCancelTransaction(transaction.id, newGasSettings));
+        break;
+      case EDIT_GAS_MODES.SPEED_UP:
+        dispatch(createSpeedUpTransaction(transaction.id, newGasSettings));
+        break;
+      case EDIT_GAS_MODES.MODIFY_IN_PLACE:
+        dispatch(
+          updateTransaction({
+            ...transaction,
+            txParams: {
+              ...transaction.txParams,
+              ...newGasSettings,
+            },
+          }),
+        );
+        break;
+      default:
+        break;
+    }
+
+    closePopover();
+  }, [
+    transaction,
+    mode,
+    dispatch,
+    closePopover,
+    gasLimit,
+    gasPrice,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+    networkSupports1559,
+  ]);
+
+  let title = t('editGasTitle');
+  if (popoverTitle) {
+    title = popoverTitle;
+  } else if (showEducationContent) {
+    title = t('editGasEducationModalTitle');
+  } else if (mode === EDIT_GAS_MODES.SPEED_UP) {
+    title = t('speedUpPopoverTitle');
+  } else if (mode === EDIT_GAS_MODES.CANCEL) {
+    title = t('cancelPopoverTitle');
+  }
+
+  const footerButtonText = confirmButtonText || t('save');
+
+  return (
+    <Popover
+      title={title}
+      onClose={closePopover}
+      onBack={
+        showEducationContent ? () => setShowEducationContent(false) : undefined
+      }
+      footer={
+        showEducationContent ? null : (
+          <>
+            <Button
+              type="primary"
+              onClick={onSubmit}
+              disabled={hasGasErrors || isGasEstimatesLoading}
+            >
+              {footerButtonText}
+            </Button>
+          </>
+        )
+      }
+    >
+      <div style={{ padding: '0 20px 20px 20px', position: 'relative' }}>
+        {showEducationContent ? (
+          <EditGasDisplayEducation />
+        ) : (
+          <>
+            <LoadingHeartBeat active={shouldAnimate} />
+            <EditGasDisplay
+              showEducationButton={showEducationButton}
+              warning={warning}
+              showAdvancedForm={showAdvancedForm}
+              setShowAdvancedForm={setShowAdvancedForm}
+              dappSuggestedGasFeeAcknowledged={dappSuggestedGasFeeAcknowledged}
+              setDappSuggestedGasFeeAcknowledged={
+                setDappSuggestedGasFeeAcknowledged
+              }
+              maxPriorityFeePerGas={maxPriorityFeePerGas}
+              setMaxPriorityFeePerGas={setMaxPriorityFeePerGas}
+              maxPriorityFeePerGasFiat={maxPriorityFeePerGasFiat}
+              maxFeePerGas={maxFeePerGas}
+              setMaxFeePerGas={setMaxFeePerGas}
+              maxFeePerGasFiat={maxFeePerGasFiat}
+              estimatedMaximumNative={estimatedMaximumNative}
+              isGasEstimatesLoading={isGasEstimatesLoading}
+              gasFeeEstimates={gasFeeEstimates}
+              gasEstimateType={gasEstimateType}
+              gasPrice={gasPrice}
+              setGasPrice={setGasPrice}
+              gasLimit={gasLimit}
+              setGasLimit={setGasLimit}
+              estimateToUse={estimateToUse}
+              setEstimateToUse={setEstimateToUse}
+              estimatedMinimumFiat={estimatedMinimumFiat}
+              estimatedMaximumFiat={estimatedMaximumFiat}
+              onEducationClick={() => setShowEducationContent(true)}
+              mode={mode}
+              transaction={transaction}
+              hasGasErrors={hasGasErrors}
+              gasErrors={gasErrors}
+              onManualChange={onManualChange}
+              {...editGasDisplayProps}
+            />
+          </>
+        )}
+      </div>
+    </Popover>
+  );
+}
+
+EditGasPopover.propTypes = {
+  popoverTitle: PropTypes.string,
+  editGasDisplayProps: PropTypes.object,
+  confirmButtonText: PropTypes.string,
+  onClose: PropTypes.func,
+  transaction: PropTypes.object,
+  mode: PropTypes.oneOf(Object.values(EDIT_GAS_MODES)),
+  defaultEstimateToUse: PropTypes.string,
+};
