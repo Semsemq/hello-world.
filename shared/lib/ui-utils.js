cf19eb0cb21de7f68f5341bbbb26623ffd309bd8
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:07:46 +0300

    Initialize e2e/dele

---
+++ shared/lib/ui-utils.js
@@ -0,0 +1,12 @@
+let _supportLink = 'https://support.metamask.io';
+
+///: BEGIN:ONLY_INCLUDE_IN(flask)
+_supportLink = 'https://metamask-flask.zendesk.com/hc';
+///: END:ONLY_INCLUDE_IN
+
+export const SUPPORT_LINK = _supportLink;
+// TODO make sure these links are correct
+export const ETHERSCAN_PRIVACY_LINK = 'https://etherscan.io/privacyPolicy';
+export const CONSENSYS_PRIVACY_LINK = 'https://consensys.net/privacy-policy/';
+export const AUTO_DETECT_TOKEN_LEARN_MORE_LINK =
+  'https://consensys.net/privacy-policy/';
