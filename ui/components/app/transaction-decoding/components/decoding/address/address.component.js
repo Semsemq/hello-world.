a73a3a485b3e8a3b509ceae70c609229cdcb19b4
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:50:18 +0300

    Initialize replace-addresses-by-the-address-component-george-updates

---
+++ ui/components/app/transaction-decoding/components/decoding/address/address.component.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import copyToClipboard from 'copy-to-clipboard';
+import { shortenAddress } from '../../../../../../helpers/utils/util';
+import Identicon from '../../../../../ui/identicon';
+import { useI18nContext } from '../../../../../../hooks/useI18nContext';
+import {
+  getMetadataContractName,
+  getAddressBook,
+} from '../../../../../../selectors';
+import NicknamePopovers from '../../../../modals/nickname-popovers';
+
+const Address = ({
+  checksummedRecipientAddress,
+  onRecipientClick,
+  addressOnly,
+  recipientEns,
+  recipientName,
+}) => {
+  const t = useI18nContext();
+  const [showNicknamePopovers, setShowNicknamePopovers] = useState(false);
+
+  const addressBook = useSelector(getAddressBook);
+  const getNickName = (address) => {
+    const addressBookEntryObject = addressBook.find((entry) => {
+      return address.toLowerCase() === entry.address.toLowerCase();
+    });
+    return (addressBookEntryObject && addressBookEntryObject?.name) || '';
+  };
+
+  const recipientNickname = getNickName(checksummedRecipientAddress);
+  const recipientMetadataName = getMetadataContractName(
+    checksummedRecipientAddress,
+  );
+
+  const recipientToRender = addressOnly
+    ? recipientName ||
+      recipientNickname ||
+      recipientMetadataName ||
+      recipientEns ||
+      shortenAddress(checksummedRecipientAddress)
+    : recipientName ||
+      recipientNickname ||
+      recipientMetadataName ||
+      recipientEns ||
+      t('newContract');
+
+  return (
+    <div
+      className="tx-insight tx-insight-component tx-insight-component-address"
+      onClick={() => {
+        copyToClipboard(checksummedRecipientAddress);
+        if (onRecipientClick) {
+          onRecipientClick();
+        }
+      }}
+    >
+      <div className="tx-insight-component-address__sender-icon">
+        <Identicon address={checksummedRecipientAddress} diameter={18} />
+      </div>
+
+      <div
+        className="address__name"
+        onClick={() => setShowNicknamePopovers(true)}
+      >
+        {recipientToRender}
+      </div>
+      {showNicknamePopovers ? (
+        <NicknamePopovers
+          onClose={() => setShowNicknamePopovers(false)}
+          address={checksummedRecipientAddress}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+Address.propTypes = {
+  checksummedRecipientAddress: PropTypes.string,
+  recipientName: PropTypes.string,
+  recipientEns: PropTypes.string,
+  addressOnly: PropTypes.bool,
+  onRecipientClick: PropTypes.func,
+};
+
+export default Address;
