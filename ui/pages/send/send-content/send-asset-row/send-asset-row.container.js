575628e530fc7fea5d32e7412abbdb66f9d30882
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:12:42 +0300

    Initialize weird-numeric-error-message-demo

---
+++ ui/pages/send/send-content/send-asset-row/send-asset-row.container.js
@@ -0,0 +1,34 @@
+import { connect } from 'react-redux';
+import {
+  getCollectibleContracts,
+  getCollectibles,
+  getNativeCurrency,
+} from '../../../../ducks/metamask/metamask';
+import {
+  getMetaMaskAccounts,
+  getNativeCurrencyImage,
+} from '../../../../selectors';
+import { updateSendAsset, getSendAsset } from '../../../../ducks/send';
+import SendAssetRow from './send-asset-row.component';
+
+function mapStateToProps(state) {
+  return {
+    tokens: state.metamask.tokens,
+    selectedAddress: state.metamask.selectedAddress,
+    collectibles: getCollectibles(state),
+    collections: getCollectibleContracts(state),
+    sendAsset: getSendAsset(state),
+    accounts: getMetaMaskAccounts(state),
+    nativeCurrency: getNativeCurrency(state),
+    nativeCurrencyImage: getNativeCurrencyImage(state),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    updateSendAsset: ({ type, details }) =>
+      dispatch(updateSendAsset({ type, details })),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SendAssetRow);
