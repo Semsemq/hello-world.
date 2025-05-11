3094002a1e82b88749795db12955a677b53cc9b7
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:16:30 +0300

    Initialize fix-permission-connect-screen

---
+++ .circleci/scripts/chrome-install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+# To get the latest version, see <https://www.ubuntuupdates.org/ppa/google_chrome?dist=stable>
+CHROME_VERSION='97.0.4692.99-1'
+CHROME_BINARY="google-chrome-stable_${CHROME_VERSION}_amd64.deb"
+CHROME_BINARY_URL="https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/${CHROME_BINARY}"
+
+# To retrieve this checksum, run the `wget` and `shasum` commands below
+CHROME_BINARY_SHA512SUM='3a97bdb9bb7a7b66188eeaff88ca6eb5bab2586225a03c4697bc075117f1f662289bc873e90001247f4addc715d86227b718e512625cc362e6d3dbc004291701'
+
+wget -O "${CHROME_BINARY}" -t 5 "${CHROME_BINARY_URL}"
+
+if [[ $(shasum -a 512 "${CHROME_BINARY}" | cut '--delimiter= ' -f1) != "${CHROME_BINARY_SHA512SUM}" ]]
+then
+  echo "Google Chrome binary checksum did not match."
+  exit 1
+else
+  echo "Google Chrome binary checksum verified."
+fi
+
+(sudo dpkg -i "${CHROME_BINARY}" || sudo apt-get -fy install)
+
+rm -rf "${CHROME_BINARY}"
+
+printf '%s\n' "CHROME ${CHROME_VERSION} configured"
