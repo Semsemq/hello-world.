e52bffeec10b301c187f969360d6079270a99edf
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:16:03 +0300

    Initialize mv3-incorrect-default-ledger-transport-type

---
+++ app/scripts/migrations/066.js
@@ -0,0 +1,36 @@
+import { cloneDeep } from 'lodash';
+import { LedgerTransportTypes } from '../../../shared/constants/hardware-wallets';
+import { getDefaultLedgerTransportType } from '../../../shared/lib/hardware-utils';
+
+const version = 66;
+
+/**
+ * Changes the useLedgerLive boolean property to the ledgerTransportType enum
+ */
+export default {
+  version,
+  async migrate(originalVersionedData) {
+    const versionedData = cloneDeep(originalVersionedData);
+    versionedData.meta.version = version;
+    const state = versionedData.data;
+    const newState = transformState(state);
+    versionedData.data = newState;
+    return versionedData;
+  },
+};
+
+function transformState(state) {
+  const defaultTransportType = getDefaultLedgerTransportType();
+  const useLedgerLive = Boolean(state.PreferencesController?.useLedgerLive);
+  const newState = {
+    ...state,
+    PreferencesController: {
+      ...state?.PreferencesController,
+      ledgerTransportType: useLedgerLive
+        ? LedgerTransportTypes.live
+        : defaultTransportType,
+    },
+  };
+  delete newState.PreferencesController.useLedgerLive;
+  return newState;
+}
---
+++ ui/pages/home/home.container.js
@@ -0,0 +1,209 @@
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import {
+  activeTabHasPermissions,
+  getFirstPermissionRequest,
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  getFirstSnapInstallOrUpdateRequest,
+  ///: END:ONLY_INCLUDE_IN
+  getIsMainnet,
+  getOriginOfCurrentTab,
+  getTotalUnapprovedCount,
+  getUnapprovedTemplatedConfirmations,
+  getWeb3ShimUsageStateForOrigin,
+  unconfirmedTransactionsCountSelector,
+  getInfuraBlocked,
+  getShowWhatsNewPopup,
+  getSortedAnnouncementsToShow,
+  getShowRecoveryPhraseReminder,
+  getShowOutdatedBrowserWarning,
+  getNewNetworkAdded,
+  hasUnsignedQRHardwareTransaction,
+  hasUnsignedQRHardwareMessage,
+  getNewCollectibleAddedMessage,
+  getNewTokensImported,
+  getShowPortfolioTooltip,
+  getShouldShowSeedPhraseReminder,
+  getRemoveCollectibleMessage,
+} from '../../selectors';
+
+import {
+  closeNotificationPopup,
+  hidePortfolioTooltip,
+  setConnectedStatusPopoverHasBeenShown,
+  setDefaultHomeActiveTabName,
+  setWeb3ShimUsageAlertDismissed,
+  setAlertEnabledness,
+  setRecoveryPhraseReminderHasBeenShown,
+  setRecoveryPhraseReminderLastShown,
+  setOutdatedBrowserWarningLastShown,
+  setNewNetworkAdded,
+  setNewCollectibleAddedMessage,
+  setRemoveCollectibleMessage,
+  setNewTokensImported,
+  setRpcTarget,
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  removeSnapError,
+  ///: END:ONLY_INCLUDE_IN
+} from '../../store/actions';
+import {
+  hideWhatsNewPopup,
+  setNewCustomNetworkAdded,
+  getPortfolioTooltipWasShownInThisSession,
+  setPortfolioTooltipWasShownInThisSession,
+} from '../../ducks/app/app';
+import { getWeb3ShimUsageAlertEnabledness } from '../../ducks/metamask/metamask';
+import { getSwapsFeatureIsLive } from '../../ducks/swaps/swaps';
+import { getEnvironmentType } from '../../../app/scripts/lib/util';
+import { getIsBrowserDeprecated } from '../../helpers/utils/util';
+import {
+  ENVIRONMENT_TYPE_NOTIFICATION,
+  ENVIRONMENT_TYPE_POPUP,
+} from '../../../shared/constants/app';
+import {
+  AlertTypes,
+  Web3ShimUsageAlertStates,
+} from '../../../shared/constants/alerts';
+import Home from './home.component';
+
+const mapStateToProps = (state) => {
+  const { metamask, appState } = state;
+  const {
+    suggestedAssets,
+    seedPhraseBackedUp,
+    selectedAddress,
+    connectedStatusPopoverHasBeenShown,
+    defaultHomeActiveTabName,
+    swapsState,
+    firstTimeFlowType,
+    completedOnboarding,
+  } = metamask;
+  const { forgottenPassword } = metamask;
+  const totalUnapprovedCount = getTotalUnapprovedCount(state);
+  const swapsEnabled = getSwapsFeatureIsLive(state);
+  const pendingConfirmations = getUnapprovedTemplatedConfirmations(state);
+
+  const envType = getEnvironmentType();
+  const isPopup = envType === ENVIRONMENT_TYPE_POPUP;
+  const isNotification = envType === ENVIRONMENT_TYPE_NOTIFICATION;
+
+  let firstPermissionsRequest, firstPermissionsRequestId;
+  firstPermissionsRequest = getFirstPermissionRequest(state);
+  firstPermissionsRequestId = firstPermissionsRequest?.metadata.id || null;
+
+  // getFirstPermissionRequest should be updated with snap update logic once we hit main extension release
+
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  if (!firstPermissionsRequest) {
+    firstPermissionsRequest = getFirstSnapInstallOrUpdateRequest(state);
+    firstPermissionsRequestId = firstPermissionsRequest?.metadata.id || null;
+  }
+  ///: END:ONLY_INCLUDE_IN
+
+  const originOfCurrentTab = getOriginOfCurrentTab(state);
+  const shouldShowWeb3ShimUsageNotification =
+    isPopup &&
+    getWeb3ShimUsageAlertEnabledness(state) &&
+    activeTabHasPermissions(state) &&
+    getWeb3ShimUsageStateForOrigin(state, originOfCurrentTab) ===
+      Web3ShimUsageAlertStates.recorded;
+
+  const isSigningQRHardwareTransaction =
+    hasUnsignedQRHardwareTransaction(state) ||
+    hasUnsignedQRHardwareMessage(state);
+
+  return {
+    forgottenPassword,
+    suggestedAssets,
+    swapsEnabled,
+    unconfirmedTransactionsCount: unconfirmedTransactionsCountSelector(state),
+    shouldShowSeedPhraseReminder: getShouldShowSeedPhraseReminder(state),
+    isPopup,
+    isNotification,
+    selectedAddress,
+    firstPermissionsRequestId,
+    totalUnapprovedCount,
+    connectedStatusPopoverHasBeenShown,
+    defaultHomeActiveTabName,
+    firstTimeFlowType,
+    completedOnboarding,
+    haveSwapsQuotes: Boolean(Object.values(swapsState.quotes || {}).length),
+    swapsFetchParams: swapsState.fetchParams,
+    showAwaitingSwapScreen: swapsState.routeState === 'awaiting',
+    isMainnet: getIsMainnet(state),
+    originOfCurrentTab,
+    shouldShowWeb3ShimUsageNotification,
+    pendingConfirmations,
+    infuraBlocked: getInfuraBlocked(state),
+    announcementsToShow: getSortedAnnouncementsToShow(state).length > 0,
+    ///: BEGIN:ONLY_INCLUDE_IN(flask)
+    errorsToShow: metamask.snapErrors,
+    shouldShowErrors: Object.entries(metamask.snapErrors || []).length > 0,
+    ///: END:ONLY_INCLUDE_IN
+    showWhatsNewPopup: getShowWhatsNewPopup(state),
+    showPortfolioTooltip: getShowPortfolioTooltip(state),
+    portfolioTooltipWasShownInThisSession:
+      getPortfolioTooltipWasShownInThisSession(state),
+    showRecoveryPhraseReminder: getShowRecoveryPhraseReminder(state),
+    showOutdatedBrowserWarning:
+      getIsBrowserDeprecated() && getShowOutdatedBrowserWarning(state),
+    seedPhraseBackedUp,
+    newNetworkAdded: getNewNetworkAdded(state),
+    isSigningQRHardwareTransaction,
+    newNftAddedMessage: getNewCollectibleAddedMessage(state),
+    removeNftMessage: getRemoveCollectibleMessage(state),
+    newTokensImported: getNewTokensImported(state),
+    newCustomNetworkAdded: appState.newCustomNetworkAdded,
+    onboardedInThisUISession: appState.onboardedInThisUISession,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  closeNotificationPopup: () => closeNotificationPopup(),
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  removeSnapError: async (id) => await removeSnapError(id),
+  ///: END:ONLY_INCLUDE_IN
+  setConnectedStatusPopoverHasBeenShown: () =>
+    dispatch(setConnectedStatusPopoverHasBeenShown()),
+  onTabClick: (name) => dispatch(setDefaultHomeActiveTabName(name)),
+  setWeb3ShimUsageAlertDismissed: (origin) =>
+    setWeb3ShimUsageAlertDismissed(origin),
+  disableWeb3ShimUsageAlert: () =>
+    setAlertEnabledness(AlertTypes.web3ShimUsage, false),
+  hideWhatsNewPopup: () => dispatch(hideWhatsNewPopup()),
+  hidePortfolioTooltip,
+  setRecoveryPhraseReminderHasBeenShown: () =>
+    dispatch(setRecoveryPhraseReminderHasBeenShown()),
+  setRecoveryPhraseReminderLastShown: (lastShown) =>
+    dispatch(setRecoveryPhraseReminderLastShown(lastShown)),
+  setOutdatedBrowserWarningLastShown: (lastShown) => {
+    dispatch(setOutdatedBrowserWarningLastShown(lastShown));
+  },
+  setNewNetworkAdded: (newNetwork) => {
+    console.log({ newNetwork });
+    dispatch(setNewNetworkAdded(newNetwork));
+  },
+  setNewCollectibleAddedMessage: (message) => {
+    dispatch(setNewCollectibleAddedMessage(message));
+  },
+  setRemoveCollectibleMessage: (message) => {
+    dispatch(setRemoveCollectibleMessage(message));
+  },
+  setNewTokensImported: (newTokens) => {
+    dispatch(setNewTokensImported(newTokens));
+  },
+  clearNewCustomNetworkAdded: () => {
+    dispatch(setNewCustomNetworkAdded({}));
+  },
+  setRpcTarget: (rpcUrl, chainId, ticker, nickname) => {
+    dispatch(setRpcTarget(rpcUrl, chainId, ticker, nickname));
+  },
+  setPortfolioTooltipWasShownInThisSession: () =>
+    dispatch(setPortfolioTooltipWasShownInThisSession()),
+});
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps, mapDispatchToProps),
+)(Home);
