56d985c33bdf456a5089d731ab92b398e716f052
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:02:22 +0300

    Initialize temporarily-disable-swaps-e2e-tests

---
+++ ui/components/app/metamask-template-renderer/safe-component-list.js
@@ -0,0 +1,48 @@
+import Button from '../../ui/button';
+import Chip from '../../ui/chip';
+import DefinitionList from '../../ui/definition-list';
+import TruncatedDefinitionList from '../../ui/truncated-definition-list';
+import Popover from '../../ui/popover';
+import Typography from '../../ui/typography';
+import Box from '../../ui/box';
+import MetaMaskTranslation from '../metamask-translation';
+import NetworkDisplay from '../network-display';
+import TextArea from '../../ui/textarea/textarea';
+import TextField from '../../ui/text-field';
+import ConfirmationNetworkSwitch from '../../../pages/confirmation/components/confirmation-network-switch';
+import UrlIcon from '../../ui/url-icon';
+import Tooltip from '../../ui/tooltip/tooltip';
+///: BEGIN:ONLY_INCLUDE_IN(flask)
+import { SnapDelineator } from '../flask/snap-delineator';
+import { Copyable } from '../flask/copyable';
+import Spinner from '../../ui/spinner';
+///: END:ONLY_INCLUDE_IN
+
+export const safeComponentList = {
+  a: 'a',
+  b: 'b',
+  i: 'i',
+  p: 'p',
+  div: 'div',
+  span: 'span',
+  Box,
+  Button,
+  Chip,
+  ConfirmationNetworkSwitch,
+  DefinitionList,
+  MetaMaskTranslation,
+  NetworkDisplay,
+  Popover,
+  TextArea,
+  TextField,
+  Tooltip,
+  TruncatedDefinitionList,
+  Typography,
+  UrlIcon,
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  SnapDelineator,
+  Copyable,
+  Spinner,
+  hr: 'hr',
+  ///: END:ONLY_INCLUDE_IN
+};
