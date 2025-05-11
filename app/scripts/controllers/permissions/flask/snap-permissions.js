21b65fa40f13dc81d52bc74cbb35f12c0fd9f06e
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:30:23 +0300

    Initialize kumavis-snap-keyring-0

---
+++ app/scripts/controllers/permissions/flask/snap-permissions.js
@@ -0,0 +1,45 @@
+import {
+  restrictedMethodPermissionBuilders,
+  selectHooks,
+} from '@metamask/rpc-methods';
+import { endowmentPermissionBuilders } from '@metamask/snaps-controllers';
+import {
+  ExcludedSnapEndowments,
+  ExcludedSnapPermissions,
+} from '../../../../../shared/constants/permissions';
+import { snapKeyringPermissionBuilders } from './snap-keyring-permissions';
+
+/**
+ * @returns {Record<string, Record<string, unknown>>} All endowment permission
+ * specifications.
+ */
+export const buildSnapEndowmentSpecifications = () =>
+  Object.values(endowmentPermissionBuilders).reduce(
+    (allSpecifications, { targetKey, specificationBuilder }) => {
+      if (!Object.keys(ExcludedSnapEndowments).includes(targetKey)) {
+        allSpecifications[targetKey] = specificationBuilder();
+      }
+      return allSpecifications;
+    },
+    {},
+  );
+
+/**
+ * @param {Record<string, Function>} hooks - The hooks for the Snap
+ * restricted method implementations.
+ */
+export const buildSnapRestrictedMethodSpecifications = (hooks) =>
+  [
+    ...Object.values(restrictedMethodPermissionBuilders),
+    ...Object.values(snapKeyringPermissionBuilders),
+  ].reduce(
+    (specifications, { targetKey, specificationBuilder, methodHooks }) => {
+      if (!Object.keys(ExcludedSnapPermissions).includes(targetKey)) {
+        specifications[targetKey] = specificationBuilder({
+          methodHooks: selectHooks(hooks, methodHooks),
+        });
+      }
+      return specifications;
+    },
+    {},
+  );
