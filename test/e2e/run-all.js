990085763aaf01f299d20e2a050b1c13a07efdf9
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:51:29 +0300

    Initialize e2e-continue-after-failure

---
+++ test/e2e/run-all.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const { promises: fs } = require('fs');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { runInShell } = require('../../development/lib/run-command');
+const { exitWithError } = require('../../development/lib/exit-with-error');
+
+async function main() {
+  const { argv } = yargs(hideBin(process.argv))
+    .usage(
+      '$0 [options]',
+      'Run all E2E tests, with a variable number of retries.',
+      (_yargs) =>
+        _yargs
+          .option('browser', {
+            description: `Set the browser used; either 'chrome' or 'firefox'.`,
+            type: 'string',
+            choices: ['chrome', 'firefox'],
+          })
+          .option('snaps', {
+            description: `run snaps e2e tests`,
+            type: 'boolean',
+          })
+          .option('retries', {
+            description:
+              'Set how many times the test should be retried upon failure.',
+            type: 'number',
+          })
+          .option('resume', {
+            description: `Continue running testcases after failure.`,
+            type: 'boolean',
+          }),
+    )
+    .strict()
+    .help('help');
+
+  const { browser, retries, snaps, resume } = argv;
+
+  let testDir = path.join(__dirname, 'tests');
+
+  if (snaps) {
+    testDir = path.join(__dirname, 'snaps');
+  }
+
+  const testFilenames = await fs.readdir(testDir);
+  const testPaths = testFilenames.map((filename) =>
+    path.join(testDir, filename),
+  );
+
+  if (!snaps) {
+    testPaths.push(path.join(__dirname, 'metamask-ui.spec.js'));
+  }
+
+  const runE2eTestPath = path.join(__dirname, 'run-e2e-test.js');
+
+  const args = [runE2eTestPath];
+  if (browser) {
+    args.push('--browser', browser);
+  }
+  if (retries) {
+    args.push('--retries', retries);
+  }
+  if (resume) {
+    args.push('--resume', resume);
+  }
+
+  for (const testPath of testPaths) {
+    try {
+      await runInShell('node', [...args, testPath]);
+    } catch (e) {
+      if (resume) {
+        continue;
+      } else {
+        break;
+      }
+    }
+  }
+}
+
+main().catch((error) => {
+  exitWithError(error);
+});
