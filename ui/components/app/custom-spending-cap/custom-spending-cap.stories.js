a7308aea0290d813d87e23d0002d19527ab36b6c
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:52:23 +0300

    Initialize token-allowance-screen-update-state-refactor

---
+++ ui/components/app/custom-spending-cap/custom-spending-cap.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import CustomSpendingCap from './custom-spending-cap';
+
+export default {
+  title: 'Components/App/CustomSpendingCap',
+  id: __filename,
+  argTypes: {
+    tokenName: {
+      control: { type: 'text' },
+    },
+    currentTokenBalance: {
+      control: { type: 'number' },
+    },
+    dappProposedValue: {
+      control: { type: 'number' },
+    },
+    siteOrigin: {
+      control: { type: 'text' },
+    },
+    passTheCurrentValue: {
+      action: 'passTheCurrentValue',
+    },
+    passTheErrorText: {
+      action: 'passTheErrorText',
+    },
+  },
+  args: {
+    tokenName: 'DAI',
+    currentTokenBalance: 200.12,
+    dappProposedValue: 7,
+    siteOrigin: 'Uniswap.org',
+  },
+};
+
+export const DefaultStory = (args) => {
+  return <CustomSpendingCap {...args} />;
+};
+
+DefaultStory.storyName = 'Default';
