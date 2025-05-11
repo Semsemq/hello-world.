94b56fe8a19d269ba2218d00d1a112d52afc0f37
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 02:22:15 +0300

    Initialize subsribe-multi

---
+++ app/scripts/lib/middleware/pending.js
@@ -0,0 +1,37 @@
+import { createAsyncMiddleware } from 'json-rpc-engine';
+import { formatTxMetaForRpcResult } from '../util';
+
+export function createPendingNonceMiddleware({ getPendingNonce }) {
+    // this would not be using the provider for the current domain...
+
+  return createAsyncMiddleware(async (req, res, next) => {
+    const { method, params } = req;
+    if (method !== 'eth_getTransactionCount') {
+      next();
+      return;
+    }
+    const [param, blockRef] = params;
+    if (blockRef !== 'pending') {
+      next();
+      return;
+    }
+    res.result = await getPendingNonce(param);
+  });
+}
+
+export function createPendingTxMiddleware({ getPendingTransactionByHash }) {
+  return createAsyncMiddleware(async (req, res, next) => {
+    const { method, params } = req;
+    if (method !== 'eth_getTransactionByHash') {
+      next();
+      return;
+    }
+    const [hash] = params;
+    const txMeta = getPendingTransactionByHash(hash);
+    if (!txMeta) {
+      next();
+      return;
+    }
+    res.result = formatTxMetaForRpcResult(txMeta);
+  });
+}
