f2a6123fa078a93216f83f8cf524131e42c0177d
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:00:12 +0300

    Initialize controllers-v2-get-persistent-state

---
+++ ui/components/app/permission-page-container/permission-page-container-content/permission-page-container-content.component.js
@@ -0,0 +1,182 @@
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { PLUGIN_PREFIX } from '@mm-snap/controllers';
+import PermissionsConnectHeader from '../../permissions-connect-header';
+import Tooltip from '../../../ui/tooltip';
+import CheckBox from '../../../ui/check-box';
+
+export default class PermissionPageContainerContent extends PureComponent {
+  static propTypes = {
+    permissionsDescriptions: PropTypes.object.isRequired,
+    domainMetadata: PropTypes.shape({
+      extensionId: PropTypes.string,
+      icon: PropTypes.string,
+      host: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      origin: PropTypes.string.isRequired,
+    }),
+    selectedPermissions: PropTypes.object.isRequired,
+    onPermissionToggle: PropTypes.func.isRequired,
+    selectedIdentities: PropTypes.array,
+    allIdentitiesSelected: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    selectedIdentities: [],
+    allIdentitiesSelected: false,
+  };
+
+  static contextTypes = {
+    t: PropTypes.func,
+  };
+
+  renderRequestedPermissions() {
+    const {
+      selectedPermissions,
+      onPermissionToggle,
+      permissionsDescriptions,
+    } = this.props;
+    const { t } = this.context;
+
+    const items = Object.keys(selectedPermissions).map((permissionName) => {
+      const isPluginPermission = permissionName.startsWith(PLUGIN_PREFIX);
+      const keyablePermissionName = isPluginPermission
+        ? `${PLUGIN_PREFIX}*`
+        : permissionName;
+      const isEthAccounts = permissionName === 'eth_accounts';
+
+      let description;
+      if (isEthAccounts) {
+        description = t(permissionName);
+      } else if (isPluginPermission) {
+        description = permissionsDescriptions[keyablePermissionName].replace(
+          '$1',
+          permissionName.replace(PLUGIN_PREFIX, ''),
+        );
+      } else {
+        description = permissionsDescriptions[keyablePermissionName];
+      }
+
+      // don't allow deselecting eth_accounts
+      const isDisabled = isEthAccounts;
+      const isChecked = Boolean(selectedPermissions[permissionName]);
+      const title = isChecked
+        ? t('permissionCheckedIconDescription')
+        : t('permissionUncheckedIconDescription');
+
+      return (
+        <div
+          className="permission-approval-container__content__permission"
+          key={permissionName}
+          onClick={() => {
+            if (!isDisabled) {
+              onPermissionToggle(permissionName);
+            }
+          }}
+        >
+          <CheckBox
+            disabled={isDisabled}
+            id={permissionName}
+            className="permission-approval-container__checkbox"
+            checked={isChecked}
+            title={title}
+          />
+          <label htmlFor={permissionName}>{description}</label>
+        </div>
+      );
+    });
+
+    return (
+      <div className="permission-approval-container__content__requested">
+        {items}
+      </div>
+    );
+  }
+
+  getAccountDescriptor(identity) {
+    return `${identity.label} (...${identity.address.slice(
+      identity.address.length - 4,
+    )})`;
+  }
+
+  renderAccountTooltip(textContent) {
+    const { selectedIdentities } = this.props;
+    const { t } = this.context;
+
+    return (
+      <Tooltip
+        key="all-account-connect-tooltip"
+        position="bottom"
+        wrapperClassName="permission-approval-container__bold-title-elements"
+        html={
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            {selectedIdentities.slice(0, 6).map((identity, index) => {
+              return (
+                <div key={`tooltip-identity-${index}`}>
+                  {this.getAccountDescriptor(identity)}
+                </div>
+              );
+            })}
+            {selectedIdentities.length > 6
+              ? t('plusXMore', [selectedIdentities.length - 6])
+              : null}
+          </div>
+        }
+      >
+        {textContent}
+      </Tooltip>
+    );
+  }
+
+  getTitle() {
+    const {
+      domainMetadata,
+      selectedIdentities,
+      allIdentitiesSelected,
+    } = this.props;
+    const { t } = this.context;
+
+    if (domainMetadata.extensionId) {
+      return t('externalExtension', [domainMetadata.extensionId]);
+    } else if (allIdentitiesSelected) {
+      return t('connectToAll', [
+        this.renderAccountTooltip(t('connectToAllAccounts')),
+      ]);
+    } else if (selectedIdentities.length > 1) {
+      return t('connectToMultiple', [
+        this.renderAccountTooltip(
+          t('connectToMultipleNumberOfAccounts', [selectedIdentities.length]),
+        ),
+      ]);
+    }
+    return t('connectTo', [this.getAccountDescriptor(selectedIdentities[0])]);
+  }
+
+  render() {
+    const { domainMetadata } = this.props;
+    const { t } = this.context;
+
+    const title = this.getTitle();
+
+    return (
+      <div className="permission-approval-container__content">
+        <div className="permission-approval-container__content-container">
+          <PermissionsConnectHeader
+            icon={domainMetadata.icon}
+            iconName={domainMetadata.name}
+            headerTitle={title}
+            headerText={
+              domainMetadata.extensionId
+                ? t('allowExternalExtensionTo', [domainMetadata.extensionId])
+                : t('allowThisSiteTo')
+            }
+            siteOrigin={domainMetadata.origin}
+          />
+          <section className="permission-approval-container__permissions-container">
+            {this.renderRequestedPermissions()}
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
