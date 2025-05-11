4ccab29d7e3e55d660e7bcff67b17cf7b4401ee0
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:35:17 +0300

    Initialize fix/18326/icon-ts-migration

---
+++ ui/components/app/custom-spending-cap/custom-spending-cap-tooltip.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '../../ui/box';
+import Typography from '../../ui/typography';
+import Tooltip from '../../ui/tooltip';
+import {
+  TextColor,
+  DISPLAY,
+  TypographyVariant,
+} from '../../../helpers/constants/design-system';
+import { Icon, IconName, IconSize } from '../../component-library';
+
+export const CustomSpendingCapTooltip = ({
+  tooltipContentText,
+  tooltipIcon,
+}) => (
+  <Box display={DISPLAY.INLINE_BLOCK}>
+    <Tooltip
+      interactive
+      position="top"
+      html={
+        <Typography
+          variant={TypographyVariant.H7}
+          margin={3}
+          color={TextColor.textAlternative}
+          className="form-field__heading-title__tooltip"
+        >
+          {tooltipContentText}
+        </Typography>
+      }
+    >
+      {tooltipIcon ? (
+        <Icon
+          name={IconName.Danger}
+          className="form-field__heading-title__tooltip__warning-icon"
+          size={IconSize.Inherit}
+          style={{ 'vertical-align': 'bottom' }}
+        />
+      ) : (
+        tooltipIcon !== '' && <Icon name={IconName.Question} />
+      )}
+    </Tooltip>
+  </Box>
+);
+
+CustomSpendingCapTooltip.propTypes = {
+  tooltipContentText: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  tooltipIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+};
