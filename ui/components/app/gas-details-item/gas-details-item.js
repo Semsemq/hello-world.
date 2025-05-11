5d71f76bdd033b400d90c88a88362fb1da0d6208
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:46:02 +0300

    Initialize v10.30.0

---
+++ ui/components/app/gas-details-item/gas-details-item.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { useSelector } from 'react-redux';
+
+import { TextColor } from '../../../helpers/constants/design-system';
+import { PRIMARY, SECONDARY } from '../../../helpers/constants/common';
+import {
+  getPreferences,
+  getUseCurrencyRateCheck,
+  transactionFeeSelector,
+} from '../../../selectors';
+import { getCurrentDraftTransaction } from '../../../ducks/send';
+import { useGasFeeContext } from '../../../contexts/gasFee';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+
+import Box from '../../ui/box';
+import LoadingHeartBeat from '../../ui/loading-heartbeat';
+import GasTiming from '../gas-timing/gas-timing.component';
+import TransactionDetailItem from '../transaction-detail-item/transaction-detail-item.component';
+import UserPreferencedCurrencyDisplay from '../user-preferenced-currency-display';
+import { hexWEIToDecGWEI } from '../../../../shared/modules/conversion.utils';
+import { useDraftTransactionGasValues } from '../../../hooks/useDraftTransactionGasValues';
+import GasDetailsItemTitle from './gas-details-item-title';
+
+const GasDetailsItem = ({ userAcknowledgedGasMissing = false }) => {
+  const t = useI18nContext();
+  const draftTransaction = useSelector(getCurrentDraftTransaction);
+  const { transactionData } = useDraftTransactionGasValues();
+
+  const {
+    hexMinimumTransactionFee: draftHexMinimumTransactionFee,
+    hexMaximumTransactionFee: draftHexMaximumTransactionFee,
+  } = useSelector((state) => transactionFeeSelector(state, transactionData));
+
+  const {
+    estimateUsed,
+    hasSimulationError,
+    maximumCostInHexWei: hexMaximumTransactionFee,
+    minimumCostInHexWei: hexMinimumTransactionFee,
+    maxPriorityFeePerGas,
+    maxFeePerGas,
+  } = useGasFeeContext();
+
+  const { useNativeCurrencyAsPrimaryCurrency } = useSelector(getPreferences);
+
+  const useCurrencyRateCheck = useSelector(getUseCurrencyRateCheck);
+
+  if (hasSimulationError && !userAcknowledgedGasMissing) {
+    return null;
+  }
+
+  return (
+    <TransactionDetailItem
+      key="gas-item"
+      detailTitle={<GasDetailsItemTitle />}
+      detailTitleColor={TextColor.textDefault}
+      detailText={
+        useCurrencyRateCheck &&
+        Object.keys(draftTransaction).length === 0 && (
+          <div className="gas-details-item__currency-container">
+            <LoadingHeartBeat estimateUsed={estimateUsed} />
+            <UserPreferencedCurrencyDisplay
+              type={SECONDARY}
+              value={hexMinimumTransactionFee}
+              hideLabel={Boolean(useNativeCurrencyAsPrimaryCurrency)}
+            />
+          </div>
+        )
+      }
+      detailTotal={
+        <div className="gas-details-item__currency-container">
+          <LoadingHeartBeat estimateUsed={estimateUsed} />
+          <UserPreferencedCurrencyDisplay
+            type={PRIMARY}
+            value={hexMinimumTransactionFee || draftHexMinimumTransactionFee}
+            hideLabel={!useNativeCurrencyAsPrimaryCurrency}
+          />
+        </div>
+      }
+      subText={
+        <>
+          <Box
+            key="editGasSubTextFeeLabel"
+            display="inline-flex"
+            className={classNames('gas-details-item__gasfee-label', {
+              'gas-details-item__gas-fee-warning': estimateUsed === 'high',
+            })}
+          >
+            <LoadingHeartBeat estimateUsed={estimateUsed} />
+            <Box marginRight={1}>
+              <strong>
+                {estimateUsed === 'high' && '⚠ '}
+                {t('editGasSubTextFeeLabel')}
+              </strong>
+            </Box>
+            <div
+              key="editGasSubTextFeeValue"
+              className="gas-details-item__currency-container"
+            >
+              <LoadingHeartBeat estimateUsed={estimateUsed} />
+              <UserPreferencedCurrencyDisplay
+                key="editGasSubTextFeeAmount"
+                type={PRIMARY}
+                value={
+                  hexMaximumTransactionFee || draftHexMaximumTransactionFee
+                }
+                hideLabel={!useNativeCurrencyAsPrimaryCurrency}
+              />
+            </div>
+          </Box>
+        </>
+      }
+      subTitle={
+        <GasTiming
+          maxPriorityFeePerGas={(
+            maxPriorityFeePerGas ||
+            hexWEIToDecGWEI(transactionData.txParams.maxPriorityFeePerGas)
+          ).toString()}
+          maxFeePerGas={(
+            maxFeePerGas ||
+            hexWEIToDecGWEI(transactionData.txParams.maxFeePerGas)
+          ).toString()}
+        />
+      }
+    />
+  );
+};
+
+GasDetailsItem.propTypes = {
+  userAcknowledgedGasMissing: PropTypes.bool,
+};
+
+export default GasDetailsItem;
---
+++ ui/components/app/wallet-overview/eth-overview.js
@@ -0,0 +1,290 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import classnames from 'classnames';
+import { useHistory } from 'react-router-dom';
+
+import Identicon from '../../ui/identicon';
+import { I18nContext } from '../../../contexts/i18n';
+import {
+  SEND_ROUTE,
+  BUILD_QUOTE_ROUTE,
+} from '../../../helpers/constants/routes';
+import Tooltip from '../../ui/tooltip';
+import UserPreferencedCurrencyDisplay from '../user-preferenced-currency-display';
+import { PRIMARY, SECONDARY } from '../../../helpers/constants/common';
+import {
+  isBalanceCached,
+  getShouldShowFiat,
+  getCurrentKeyring,
+  getSwapsDefaultToken,
+  getIsSwapsChain,
+  getIsBridgeChain,
+  getIsBuyableChain,
+  getNativeCurrencyImage,
+  getSelectedAccountCachedBalance,
+} from '../../../selectors';
+import { setSwapsFromToken } from '../../../ducks/swaps/swaps';
+import IconButton from '../../ui/icon-button';
+import { isHardwareKeyring } from '../../../helpers/utils/hardware';
+import { MetaMetricsContext } from '../../../contexts/metametrics';
+import {
+  MetaMetricsContextProp,
+  MetaMetricsEventCategory,
+  MetaMetricsEventName,
+  MetaMetricsSwapsEventSource,
+} from '../../../../shared/constants/metametrics';
+import Spinner from '../../ui/spinner';
+import { startNewDraftTransaction } from '../../../ducks/send';
+import { AssetType } from '../../../../shared/constants/transaction';
+import {
+  ButtonIcon,
+  ButtonIconSize,
+  Icon,
+  IconName,
+} from '../../component-library';
+import { IconColor } from '../../../helpers/constants/design-system';
+import useRamps from '../../../hooks/experiences/useRamps';
+import WalletOverview from './wallet-overview';
+
+const EthOverview = ({ className }) => {
+  const dispatch = useDispatch();
+  const t = useContext(I18nContext);
+  const trackEvent = useContext(MetaMetricsContext);
+  const history = useHistory();
+  const keyring = useSelector(getCurrentKeyring);
+  const usingHardwareWallet = isHardwareKeyring(keyring?.type);
+  const balanceIsCached = useSelector(isBalanceCached);
+  const showFiat = useSelector(getShouldShowFiat);
+  const balance = useSelector(getSelectedAccountCachedBalance);
+  const isSwapsChain = useSelector(getIsSwapsChain);
+  const isBridgeChain = useSelector(getIsBridgeChain);
+  const isBuyableChain = useSelector(getIsBuyableChain);
+  const primaryTokenImage = useSelector(getNativeCurrencyImage);
+  const defaultSwapsToken = useSelector(getSwapsDefaultToken);
+
+  const { openBuyCryptoInPdapp } = useRamps();
+
+  return (
+    <WalletOverview
+      loading={!balance}
+      balance={
+        <Tooltip
+          position="top"
+          title={t('balanceOutdated')}
+          disabled={!balanceIsCached}
+        >
+          <div className="eth-overview__balance">
+            <div className="eth-overview__primary-container">
+              {balance ? (
+                <UserPreferencedCurrencyDisplay
+                  style={{ display: 'contents' }}
+                  className={classnames('eth-overview__primary-balance', {
+                    'eth-overview__cached-balance': balanceIsCached,
+                  })}
+                  data-testid="eth-overview__primary-currency"
+                  value={balance}
+                  type={PRIMARY}
+                  ethNumberOfDecimals={4}
+                  hideTitle
+                />
+              ) : (
+                <Spinner
+                  color="var(--color-secondary-default)"
+                  className="loading-overlay__spinner"
+                />
+              )}
+              {balanceIsCached ? (
+                <span className="eth-overview__cached-star">*</span>
+              ) : null}
+              {process.env.MULTICHAIN ? null : (
+                <ButtonIcon
+                  className="eth-overview__portfolio-button"
+                  data-testid="home__portfolio-site"
+                  color={IconColor.primaryDefault}
+                  iconName={IconName.Diagram}
+                  ariaLabel={t('portfolio')}
+                  size={ButtonIconSize.Lg}
+                  onClick={() => {
+                    const portfolioUrl = process.env.PORTFOLIO_URL;
+                    global.platform.openTab({
+                      url: `${portfolioUrl}?metamaskEntry=ext`,
+                    });
+                    trackEvent(
+                      {
+                        category: MetaMetricsEventCategory.Home,
+                        event: MetaMetricsEventName.PortfolioLinkClicked,
+                        properties: {
+                          url: portfolioUrl,
+                        },
+                      },
+                      {
+                        contextPropsIntoEventProperties: [
+                          MetaMetricsContextProp.PageTitle,
+                        ],
+                      },
+                    );
+                  }}
+                />
+              )}
+            </div>
+            {showFiat && balance && (
+              <UserPreferencedCurrencyDisplay
+                className={classnames({
+                  'eth-overview__cached-secondary-balance': balanceIsCached,
+                  'eth-overview__secondary-balance': !balanceIsCached,
+                })}
+                data-testid="eth-overview__secondary-currency"
+                value={balance}
+                type={SECONDARY}
+                ethNumberOfDecimals={4}
+                hideTitle
+              />
+            )}
+          </div>
+        </Tooltip>
+      }
+      buttons={
+        <>
+          <IconButton
+            className="eth-overview__button"
+            Icon={<Icon name={IconName.Add} color={IconColor.primaryInverse} />}
+            disabled={!isBuyableChain}
+            data-testid="eth-overview-buy"
+            label={t('buy')}
+            onClick={() => {
+              openBuyCryptoInPdapp();
+              trackEvent({
+                event: MetaMetricsEventName.NavBuyButtonClicked,
+                category: MetaMetricsEventCategory.Navigation,
+                properties: {
+                  location: 'Home',
+                  text: 'Buy',
+                },
+              });
+            }}
+          />
+          <IconButton
+            className="eth-overview__button"
+            data-testid="eth-overview-send"
+            Icon={
+              <Icon
+                name={IconName.Arrow2UpRight}
+                color={IconColor.primaryInverse}
+              />
+            }
+            label={t('send')}
+            onClick={() => {
+              trackEvent({
+                event: MetaMetricsEventName.NavSendButtonClicked,
+                category: MetaMetricsEventCategory.Navigation,
+                properties: {
+                  token_symbol: 'ETH',
+                  location: 'Home',
+                  text: 'Send',
+                },
+              });
+              dispatch(
+                startNewDraftTransaction({ type: AssetType.native }),
+              ).then(() => {
+                history.push(SEND_ROUTE);
+              });
+            }}
+          />
+          <IconButton
+            className="eth-overview__button"
+            disabled={!isSwapsChain}
+            Icon={
+              <Icon
+                name={IconName.SwapHorizontal}
+                color={IconColor.primaryInverse}
+              />
+            }
+            onClick={() => {
+              if (isSwapsChain) {
+                trackEvent({
+                  event: MetaMetricsEventName.NavSwapButtonClicked,
+                  category: MetaMetricsEventCategory.Swaps,
+                  properties: {
+                    token_symbol: 'ETH',
+                    location: MetaMetricsSwapsEventSource.MainView,
+                    text: 'Swap',
+                  },
+                });
+                dispatch(setSwapsFromToken(defaultSwapsToken));
+                if (usingHardwareWallet) {
+                  global.platform.openExtensionInBrowser(BUILD_QUOTE_ROUTE);
+                } else {
+                  history.push(BUILD_QUOTE_ROUTE);
+                }
+              }
+            }}
+            label={t('swap')}
+            tooltipRender={
+              isSwapsChain
+                ? null
+                : (contents) => (
+                    <Tooltip
+                      title={t('currentlyUnavailable')}
+                      position="bottom"
+                    >
+                      {contents}
+                    </Tooltip>
+                  )
+            }
+          />
+          <IconButton
+            className="eth-overview__button"
+            disabled={!isBridgeChain}
+            data-testid="eth-overview-bridge"
+            Icon={
+              <Icon name={IconName.Bridge} color={IconColor.primaryInverse} />
+            }
+            label={t('bridge')}
+            onClick={() => {
+              if (isBridgeChain) {
+                const portfolioUrl = process.env.PORTFOLIO_URL;
+                const bridgeUrl = `${portfolioUrl}/bridge`;
+                global.platform.openTab({
+                  url: `${bridgeUrl}?metamaskEntry=ext`,
+                });
+                trackEvent({
+                  category: MetaMetricsEventCategory.Navigation,
+                  event: MetaMetricsEventName.BridgeLinkClicked,
+                  properties: {
+                    location: 'Home',
+                    text: 'Bridge',
+                  },
+                });
+              }
+            }}
+            tooltipRender={
+              isBridgeChain
+                ? null
+                : (contents) => (
+                    <Tooltip
+                      title={t('currentlyUnavailable')}
+                      position="bottom"
+                    >
+                      {contents}
+                    </Tooltip>
+                  )
+            }
+          />
+        </>
+      }
+      className={className}
+      icon={<Identicon diameter={32} image={primaryTokenImage} />}
+    />
+  );
+};
+
+EthOverview.propTypes = {
+  className: PropTypes.string,
+};
+
+EthOverview.defaultProps = {
+  className: undefined,
+};
+
+export default EthOverview;
