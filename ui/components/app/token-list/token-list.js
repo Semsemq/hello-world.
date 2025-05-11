22b953117b2fd05d57b5bdbbcc9f9e31a6bbe642
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 01:33:41 +0300

    Initialize MMASSETS-11

---
+++ ui/components/app/token-list/token-list.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { isEqual } from 'lodash';
+
+import { useSelector } from 'react-redux';
+import TokenCell from '../token-cell';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+import { useTokenTracker } from '../../../hooks/useTokenTracker';
+import { getShouldHideZeroBalanceTokens } from '../../../selectors';
+import { getTokens } from '../../../ducks/metamask/metamask';
+import { Box } from '../../component-library';
+import {
+  AlignItems,
+  Display,
+  JustifyContent,
+} from '../../../helpers/constants/design-system';
+
+export default function TokenList({ onTokenClick }) {
+  const t = useI18nContext();
+  const shouldHideZeroBalanceTokens = useSelector(
+    getShouldHideZeroBalanceTokens,
+  );
+  // use `isEqual` comparison function because the token array is serialized
+  // from the background so it has a new reference with each background update,
+  // even if the tokens haven't changed
+  const tokens = useSelector(getTokens, isEqual);
+  const { loading, tokensWithBalances } = useTokenTracker(
+    tokens,
+    true,
+    shouldHideZeroBalanceTokens,
+  );
+  if (loading) {
+    return (
+      <Box
+        display={Display.Flex}
+        alignItems={AlignItems.center}
+        justifyContent={JustifyContent.center}
+        padding={7}
+        data-testid="token-list-loading-message"
+      >
+        {t('loadingTokens')}
+      </Box>
+    );
+  }
+
+  const sortedTokens = tokensWithBalances.sort((a, b) => {
+    const aBalance = a.balance || 0;
+    const bBalance = b.balance || 0;
+    return bBalance - aBalance;
+  });
+
+  return (
+    <div>
+      {sortedTokens.map((tokenData, index) => {
+        return <TokenCell key={index} {...tokenData} onClick={onTokenClick} />;
+      })}
+    </div>
+  );
+}
+
+TokenList.propTypes = {
+  onTokenClick: PropTypes.func.isRequired,
+};
