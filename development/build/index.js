c5c93494726899641164fba30c5a4b56f8eca27b
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-11 23:05:43 +0300

    Initialize e2e-fetch-mocking

---
+++ development/build/index.js
@@ -0,0 +1,190 @@
+//
+// build task definitions
+//
+// run any task with "yarn build ${taskName}"
+//
+const livereload = require('gulp-livereload');
+const minimist = require('minimist');
+const {
+  createTask,
+  composeSeries,
+  composeParallel,
+  runTask,
+} = require('./task');
+const createManifestTasks = require('./manifest');
+const createScriptTasks = require('./scripts');
+const createStyleTasks = require('./styles');
+const createStaticAssetTasks = require('./static');
+const createEtcTasks = require('./etc');
+const { BuildTypes, getBrowserVersionMap } = require('./utils');
+
+// packages required dynamically via browserify configuration in dependencies
+require('loose-envify');
+require('@babel/plugin-proposal-object-rest-spread');
+require('@babel/plugin-transform-runtime');
+require('@babel/plugin-proposal-class-properties');
+require('@babel/plugin-proposal-optional-chaining');
+require('@babel/plugin-proposal-nullish-coalescing-operator');
+require('@babel/preset-env');
+require('@babel/preset-react');
+require('@babel/core');
+
+defineAndRunBuildTasks();
+
+function defineAndRunBuildTasks() {
+  const {
+    buildType,
+    entryTask,
+    isLavaMoat,
+    shouldIncludeLockdown,
+    shouldLintFenceFiles,
+    skipStats,
+  } = parseArgv();
+
+  const browserPlatforms = ['firefox', 'chrome', 'brave', 'opera'];
+
+  const browserVersionMap = getBrowserVersionMap(browserPlatforms);
+
+  const staticTasks = createStaticAssetTasks({
+    livereload,
+    browserPlatforms,
+    shouldIncludeLockdown,
+    buildType,
+  });
+
+  const manifestTasks = createManifestTasks({
+    browserPlatforms,
+    browserVersionMap,
+    buildType,
+  });
+
+  const styleTasks = createStyleTasks({ livereload });
+
+  const scriptTasks = createScriptTasks({
+    browserPlatforms,
+    buildType,
+    isLavaMoat,
+    livereload,
+    shouldLintFenceFiles,
+  });
+
+  const { clean, reload, zip } = createEtcTasks({
+    livereload,
+    browserPlatforms,
+    buildType,
+  });
+
+  // build for development (livereload)
+  createTask(
+    'dev',
+    composeSeries(
+      clean,
+      styleTasks.dev,
+      composeParallel(
+        scriptTasks.dev,
+        staticTasks.dev,
+        manifestTasks.dev,
+        reload,
+      ),
+    ),
+  );
+
+  // build for test development (livereload)
+  createTask(
+    'testDev',
+    composeSeries(
+      clean,
+      styleTasks.dev,
+      composeParallel(
+        scriptTasks.testDev,
+        staticTasks.testDev,
+        manifestTasks.testDev,
+        reload,
+      ),
+    ),
+  );
+
+  // build for prod release
+  createTask(
+    'prod',
+    composeSeries(
+      clean,
+      styleTasks.prod,
+      composeParallel(scriptTasks.prod, staticTasks.prod, manifestTasks.prod),
+      zip,
+    ),
+  );
+
+  // build for CI testing
+  createTask(
+    'test',
+    composeSeries(
+      clean,
+      styleTasks.prod,
+      composeParallel(scriptTasks.test, staticTasks.test, manifestTasks.test),
+      zip,
+    ),
+  );
+
+  // special build for minimal CI testing
+  createTask('styles', styleTasks.prod);
+
+  // Finally, start the build process by running the entry task.
+  runTask(entryTask, { skipStats });
+}
+
+function parseArgv() {
+  const NamedArgs = {
+    BuildType: 'build-type',
+    LintFenceFiles: 'lint-fence-files',
+    OmitLockdown: 'omit-lockdown',
+    SkipStats: 'skip-stats',
+  };
+
+  const argv = minimist(process.argv.slice(2), {
+    boolean: [
+      NamedArgs.LintFenceFiles,
+      NamedArgs.OmitLockdown,
+      NamedArgs.SkipStats,
+    ],
+    string: [NamedArgs.BuildType],
+    default: {
+      [NamedArgs.BuildType]: BuildTypes.main,
+      [NamedArgs.LintFenceFiles]: true,
+      [NamedArgs.OmitLockdown]: false,
+      [NamedArgs.SkipStats]: false,
+    },
+  });
+
+  if (argv._.length !== 1) {
+    throw new Error(
+      `Metamask build: Expected a single positional argument, but received "${argv._.length}" arguments.`,
+    );
+  }
+
+  const entryTask = argv._[0];
+  if (!entryTask) {
+    throw new Error('MetaMask build: No entry task specified.');
+  }
+
+  const buildType = argv[NamedArgs.BuildType];
+  if (!(buildType in BuildTypes)) {
+    throw new Error(`MetaMask build: Invalid build type: "${buildType}"`);
+  }
+
+  // Manually default this to `false` for dev builds only.
+  const shouldLintFenceFiles = process.argv.includes(
+    `--${NamedArgs.LintFenceFiles}`,
+  )
+    ? argv[NamedArgs.LintFenceFiles]
+    : !/dev/iu.test(entryTask);
+
+  return {
+    buildType,
+    entryTask,
+    isLavaMoat: process.argv[0].includes('lavamoat'),
+    shouldIncludeLockdown: argv[NamedArgs.OmitLockdown],
+    shouldLintFenceFiles,
+    skipStats: argv[NamedArgs.SkipStats],
+  };
+}
---
+++ test/e2e/webdriver/index.js
@@ -0,0 +1,41 @@
+const { Browser } = require('selenium-webdriver');
+const Driver = require('./driver');
+const ChromeDriver = require('./chrome');
+const FirefoxDriver = require('./firefox');
+
+async function buildWebDriver({ responsive, port } = {}) {
+  const browser = process.env.SELENIUM_BROWSER;
+
+  const {
+    driver: seleniumDriver,
+    extensionId,
+    extensionUrl,
+  } = await buildBrowserWebDriver(browser, { responsive, port });
+  const driver = new Driver(seleniumDriver, browser, extensionUrl);
+
+  // Wait for fetch mock service worker to start, and for background to initialize
+  await driver.delay(2000);
+
+  return {
+    driver,
+    extensionId,
+  };
+}
+
+async function buildBrowserWebDriver(browser, webDriverOptions) {
+  switch (browser) {
+    case Browser.CHROME: {
+      return await ChromeDriver.build(webDriverOptions);
+    }
+    case Browser.FIREFOX: {
+      return await FirefoxDriver.build(webDriverOptions);
+    }
+    default: {
+      throw new Error(`Unrecognized browser: ${browser}`);
+    }
+  }
+}
+
+module.exports = {
+  buildWebDriver,
+};
