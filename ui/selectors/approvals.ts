9a3ce14d6dc265dbb64d853514eb59190a98d324
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 01:02:35 +0300

    Initialize approval-flow-loading-text

---
+++ ui/selectors/approvals.ts
@@ -0,0 +1,36 @@
+import { ApprovalControllerState } from '@metamask/approval-controller';
+import { ApprovalType } from '@metamask/controller-utils';
+
+type ApprovalsMetaMaskState = {
+  metamask: {
+    pendingApprovals: ApprovalControllerState['pendingApprovals'];
+    approvalFlows: ApprovalControllerState['approvalFlows'];
+  };
+};
+
+export const getApprovalRequestsByType = (
+  state: ApprovalsMetaMaskState,
+  approvalType: ApprovalType,
+) => {
+  const pendingApprovalRequests = Object.values(
+    state.metamask.pendingApprovals,
+  ).filter(({ type }) => type === approvalType);
+
+  return pendingApprovalRequests;
+};
+
+export function hasPendingApprovals(
+  state: ApprovalsMetaMaskState,
+  approvalType: ApprovalType,
+) {
+  const pendingApprovalRequests = getApprovalRequestsByType(
+    state,
+    approvalType,
+  );
+
+  return pendingApprovalRequests.length > 0;
+}
+
+export function getPendingApprovalFlows(state: ApprovalsMetaMaskState) {
+  return state.metamask.approvalFlows;
+}
