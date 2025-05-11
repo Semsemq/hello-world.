1f79a1b0eb8dfb03540c42f330c0b0e6eca49d70
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:34:14 +0300

    Initialize show-new-header-on-siwe-screen

---
+++ ui/components/app/signature-request-siwe/signature-request-siwe.js
@@ -0,0 +1,250 @@
+import React, { useCallback, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import log from 'loglevel';
+import { BannerAlert, Text } from '../../component-library';
+import Popover from '../../ui/popover';
+import Checkbox from '../../ui/check-box';
+import { I18nContext } from '../../../contexts/i18n';
+import { PageContainerFooter } from '../../ui/page-container';
+import {
+  accountsWithSendEtherInfoSelector,
+  conversionRateSelector,
+  getSubjectMetadata,
+  getCurrentCurrency,
+  getCurrentChainId,
+  getPreferences,
+  getProvider,
+} from '../../../selectors';
+import {
+  getAccountByAddress,
+  getNetworkName,
+} from '../../../helpers/utils/util';
+import { formatMessageParams } from '../../../../shared/modules/siwe';
+import {
+  SEVERITIES,
+  TextVariant,
+} from '../../../helpers/constants/design-system';
+
+import SecurityProviderBannerMessage from '../security-provider-banner-message/security-provider-banner-message';
+import { SECURITY_PROVIDER_MESSAGE_SEVERITIES } from '../security-provider-banner-message/security-provider-banner-message.constants';
+import NetworkAccountBalanceHeader from '../network-account-balance-header/network-account-balance-header';
+import { getNativeCurrency } from '../../../ducks/metamask/metamask';
+import { Numeric } from '../../../../shared/modules/Numeric';
+import { EtherDenomination } from '../../../../shared/constants/common';
+import { formatCurrency } from '../../../helpers/utils/confirm-tx.util';
+import { getValueFromWeiHex } from '../../../../shared/modules/conversion.utils';
+import Message from './signature-request-siwe-message';
+import Header from './signature-request-siwe-header';
+
+export default function SignatureRequestSIWE({
+  txData,
+  cancelPersonalMessage,
+  signPersonalMessage,
+}) {
+  const allAccounts = useSelector(accountsWithSendEtherInfoSelector);
+  const subjectMetadata = useSelector(getSubjectMetadata);
+  const nativeCurrency = useSelector(getNativeCurrency);
+  const currentCurrency = useSelector(getCurrentCurrency);
+  const currentChainId = useSelector(getCurrentChainId);
+  const provider = useSelector(getProvider);
+
+  const {
+    msgParams: {
+      from,
+      origin,
+      siwe: { parsedMessage },
+    },
+  } = txData;
+
+  const fromAccount = getAccountByAddress(allAccounts, from);
+  const targetSubjectMetadata = subjectMetadata[origin];
+
+  const t = useContext(I18nContext);
+
+  const isMatchingAddress =
+    from.toLowerCase() === parsedMessage.address.toLowerCase();
+
+  const checkSIWEDomain = () => {
+    let isSIWEDomainValid = false;
+
+    if (origin) {
+      const { host } = new URL(origin);
+      isSIWEDomainValid = parsedMessage.domain === host;
+    }
+    return isSIWEDomainValid;
+  };
+
+  const isSIWEDomainValid = checkSIWEDomain();
+
+  const [isShowingDomainWarning, setIsShowingDomainWarning] = useState(false);
+  const [agreeToDomainWarning, setAgreeToDomainWarning] = useState(false);
+
+  const onSign = useCallback(
+    async (event) => {
+      try {
+        await signPersonalMessage(event);
+      } catch (e) {
+        log.error(e);
+      }
+    },
+    [signPersonalMessage],
+  );
+
+  const onCancel = useCallback(
+    async (event) => {
+      try {
+        await cancelPersonalMessage(event);
+      } catch (e) {
+        log.error(e);
+      }
+    },
+    [cancelPersonalMessage],
+  );
+
+  const { useNativeCurrencyAsPrimaryCurrency } = useSelector(getPreferences);
+
+  const conversionRateFromSelector = useSelector(conversionRateSelector);
+
+  const conversionRate = useNativeCurrencyAsPrimaryCurrency
+    ? null
+    : conversionRateFromSelector;
+
+  const balanceInBaseAsset = conversionRate
+    ? formatCurrency(
+        getValueFromWeiHex({
+          value: fromAccount.balance,
+          fromCurrency: nativeCurrency,
+          toCurrency: currentCurrency,
+          conversionRate,
+          numberOfDecimals: 6,
+          toDenomination: EtherDenomination.ETH,
+        }),
+        currentCurrency,
+      )
+    : new Numeric(fromAccount.balance, 16, EtherDenomination.WEI)
+        .toDenomination(EtherDenomination.ETH)
+        .round(6)
+        .toBase(10)
+        .toString();
+
+  const currentNetwork = getNetworkName(provider, t);
+
+  return (
+    <div className="signature-request-siwe">
+      <NetworkAccountBalanceHeader
+        networkName={currentNetwork}
+        accountName={fromAccount.name}
+        accountBalance={balanceInBaseAsset}
+        tokenName={
+          conversionRate ? currentCurrency?.toUpperCase() : nativeCurrency
+        }
+        accountAddress={fromAccount.address}
+        chainId={currentChainId}
+      />
+      <Header
+        fromAccount={fromAccount}
+        domain={origin}
+        isSIWEDomainValid={isSIWEDomainValid}
+        subjectMetadata={targetSubjectMetadata}
+      />
+      {(txData?.securityProviderResponse?.flagAsDangerous !== undefined &&
+        txData?.securityProviderResponse?.flagAsDangerous !==
+          SECURITY_PROVIDER_MESSAGE_SEVERITIES.NOT_MALICIOUS) ||
+      (txData?.securityProviderResponse &&
+        Object.keys(txData.securityProviderResponse).length === 0) ? (
+        <SecurityProviderBannerMessage
+          securityProviderResponse={txData.securityProviderResponse}
+        />
+      ) : null}
+      <Message data={formatMessageParams(parsedMessage, t)} />
+      {!isMatchingAddress && (
+        <BannerAlert
+          severity={SEVERITIES.WARNING}
+          marginLeft={4}
+          marginRight={4}
+          marginBottom={4}
+        >
+          {t('SIWEAddressInvalid', [
+            parsedMessage.address,
+            fromAccount.address,
+          ])}
+        </BannerAlert>
+      )}
+      {!isSIWEDomainValid && (
+        <BannerAlert
+          severity={SEVERITIES.DANGER}
+          marginLeft={4}
+          marginRight={4}
+          marginBottom={4}
+        >
+          <Text variant={TextVariant.bodyMdBold}>
+            {t('SIWEDomainInvalidTitle')}
+          </Text>{' '}
+          <Text>{t('SIWEDomainInvalidText')}</Text>
+        </BannerAlert>
+      )}
+      <PageContainerFooter
+        footerClassName="signature-request-siwe__page-container-footer"
+        onCancel={onCancel}
+        onSubmit={
+          isSIWEDomainValid ? onSign : () => setIsShowingDomainWarning(true)
+        }
+        cancelText={t('cancel')}
+        submitText={t('signin')}
+        submitButtonType={isSIWEDomainValid ? 'primary' : 'danger-primary'}
+      />
+      {isShowingDomainWarning && (
+        <Popover
+          onClose={() => setIsShowingDomainWarning(false)}
+          title={t('SIWEWarningTitle')}
+          subtitle={t('SIWEWarningSubtitle')}
+          className="signature-request-siwe__warning-popover"
+          footerClassName="signature-request-siwe__warning-popover__footer"
+          footer={
+            <PageContainerFooter
+              footerClassName="signature-request-siwe__warning-popover__footer__warning-footer"
+              onCancel={() => setIsShowingDomainWarning(false)}
+              cancelText={t('cancel')}
+              cancelButtonType="default"
+              onSubmit={onSign}
+              submitText={t('confirm')}
+              submitButtonType="danger-primary"
+              disabled={!agreeToDomainWarning}
+            />
+          }
+        >
+          <div className="signature-request-siwe__warning-popover__checkbox-wrapper">
+            <Checkbox
+              id="signature-request-siwe_domain-checkbox"
+              checked={agreeToDomainWarning}
+              className="signature-request-siwe__warning-popover__checkbox-wrapper__checkbox"
+              onClick={() => setAgreeToDomainWarning((checked) => !checked)}
+            />
+            <label
+              className="signature-request-siwe__warning-popover__checkbox-wrapper__label"
+              htmlFor="signature-request-siwe_domain-checkbox"
+            >
+              {t('SIWEDomainWarningBody', [parsedMessage.domain])}
+            </label>
+          </div>
+        </Popover>
+      )}
+    </div>
+  );
+}
+
+SignatureRequestSIWE.propTypes = {
+  /**
+   * The display content of transaction data
+   */
+  txData: PropTypes.object.isRequired,
+  /**
+   * Handler for cancel button
+   */
+  cancelPersonalMessage: PropTypes.func.isRequired,
+  /**
+   * Handler for sign button
+   */
+  signPersonalMessage: PropTypes.func.isRequired,
+};
