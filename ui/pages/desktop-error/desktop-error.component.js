7f37132a803eafc65d89e1f1ca6d26317593cb50
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 02:25:06 +0300

    Initialize jl/mmp-1023/metametrics-controller

---
+++ ui/pages/desktop-error/desktop-error.component.js
@@ -0,0 +1,37 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useContext } from 'react';
+import { useI18nContext } from '../../hooks/useI18nContext';
+import {
+  downloadDesktopApp,
+  downloadExtension,
+  restartExtension,
+} from '../../../shared/lib/error-utils';
+import { DEFAULT_ROUTE } from '../../helpers/constants/routes';
+import { MetaMetricsContext } from '../../contexts/metametrics';
+import { renderDesktopError } from './render-desktop-error';
+
+export default function DesktopError({ forceDisableDesktop }) {
+  const t = useI18nContext();
+  const { errorType } = useParams();
+  const history = useHistory();
+  const {trackEvent, trackPage} = useContext(MetaMetricsContext);
+
+  useEffect(() => {
+    trackPage() // does this need chainId?
+  }, [])
+
+  return renderDesktopError({
+    type: errorType,
+    t,
+    isHtmlError: false,
+    history,
+    disableDesktop: () => {
+      forceDisableDesktop();
+      history.push(DEFAULT_ROUTE);
+    },
+    downloadDesktopApp,
+    downloadExtension,
+    restartExtension,
+    trackEvent,
+  });
+}
