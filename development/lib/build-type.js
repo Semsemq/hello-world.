3a741c3c0803354a175efb9c489a3c4cc0bd4160
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 00:19:26 +0300

    Initialize update-json-rpc-packages

---
+++ development/lib/build-type.js
@@ -0,0 +1,17 @@
+/**
+ * The distribution this build is intended for.
+ *
+ * This should be kept in-sync with the `BuildType` map in `shared/constants/app.js`.
+ */
+const BuildType = {
+  beta: 'beta',
+  desktop: 'desktop',
+  flask: 'flask',
+  main: 'main',
+};
+
+const BuildTypeInheritance = {
+  [BuildType.desktop]: [BuildType.flask],
+};
+
+module.exports = { BuildType, BuildTypeInheritance };
