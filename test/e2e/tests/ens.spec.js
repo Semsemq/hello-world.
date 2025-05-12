96b85491dda8f3d0536fc1a65d0726829636d8df
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2025-05-12 03:21:01 +0300

    Initialize send-flow-ens-e2e

---
+++ test/e2e/tests/ens.spec.js
@@ -0,0 +1,181 @@
+const {
+  defaultGanacheOptions,
+  withFixtures,
+  openActionMenuAndStartSendFlow,
+  unlockWallet,
+} = require('../helpers');
+const FixtureBuilder = require('../fixture-builder');
+
+describe('ENS', function () {
+  const sampleAddress = '1111111111111111111111111111111111111111';
+  const sampleAddress2 = '2222222222222222222222222222222222222222';
+
+  const sampleEnsDomain = 'test.eth';
+  const infuraUrl =
+    'https://mainnet.infura.io/v3/00000000000000000000000000000000';
+
+  const infuraUrl2 =
+    'https://mainnet.infura.io/v3/00000000000000000000000000000002';
+
+  async function mockInfura(mockServer) {
+    await mockServer
+      .forPost(infuraUrl)
+      .withJsonBodyIncluding({ method: 'eth_blockNumber' })
+      .thenCallback(() => {
+        return {
+          statusCode: 200,
+          json: {
+            jsonrpc: '2.0',
+            id: '1111111111111111',
+            result: '0x1',
+          },
+        };
+      });
+
+    await mockServer
+      .forPost(infuraUrl)
+      .withJsonBodyIncluding({ method: 'eth_getBalance' })
+      .thenCallback(() => {
+        return {
+          statusCode: 200,
+          json: {
+            jsonrpc: '2.0',
+            id: '1111111111111111',
+            result: '0x1',
+          },
+        };
+      });
+
+    await mockServer
+      .forPost(infuraUrl)
+      .withJsonBodyIncluding({ method: 'eth_getBlockByNumber' })
+      .thenCallback(() => {
+        return {
+          statusCode: 200,
+          json: {
+            jsonrpc: '2.0',
+            id: '1111111111111111',
+            result: {},
+          },
+        };
+      });
+
+    await mockServer
+      .forPost(infuraUrl)
+      .withJsonBodyIncluding({ method: 'eth_call' })
+      .thenCallback(() => {
+        return {
+          statusCode: 200,
+          json: {
+            jsonrpc: '2.0',
+            id: '1111111111111111',
+            result: `0x000000000000000000000000${sampleAddress}`,
+          },
+        };
+      });
+
+    await mockServer
+      .forPost(infuraUrl2)
+      .withJsonBodyIncluding({ method: 'eth_call' })
+      .thenCallback(() => {
+        return {
+          statusCode: 200,
+          json: {
+            jsonrpc: '2.0',
+            id: '1111111111111111',
+            result: `0x000000000000000000000000${sampleAddress2}`,
+          },
+        };
+      });
+  }
+
+  it('domain resolves to a correct address', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test.fullTitle(),
+        testSpecificMock: mockInfura,
+      },
+      async ({ driver }) => {
+        await unlockWallet(driver);
+
+        await driver.assertElementNotPresent('.loading-overlay');
+
+        await openActionMenuAndStartSendFlow(driver);
+
+        await driver.pasteIntoField(
+          'input[placeholder="Enter public address (0x) or ENS name"]',
+          sampleEnsDomain,
+        );
+
+        await driver.waitForSelector({
+          text: sampleEnsDomain,
+          css: '[data-testid="address-list-item-label"]',
+        });
+
+        await driver.clickElement('.address-list-item');
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__title',
+          text: 'test.eth',
+        });
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__subtitle',
+          text: `0x${sampleAddress}`,
+        });
+      },
+    );
+  });
+
+  it.only('domain resolves to a correct address after changing network', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test.fullTitle(),
+        testSpecificMock: mockInfura,
+      },
+      async ({ driver }) => {
+        await unlockWallet(driver);
+
+        await driver.assertElementNotPresent('.loading-overlay');
+
+        await openActionMenuAndStartSendFlow(driver);
+
+        await driver.pasteIntoField(
+          'input[placeholder="Enter public address (0x) or ENS name"]',
+          sampleEnsDomain,
+        );
+
+        await driver.waitForSelector({
+          text: sampleEnsDomain,
+          css: '[data-testid="address-list-item-label"]',
+        });
+
+        await driver.clickElement('.address-list-item');
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__title',
+          text: 'test.eth',
+        });
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__subtitle',
+          text: `0x${sampleAddress}`,
+        });
+
+        // Change network
+        await driver.clickElement('[data-testid="send-page-network-picker"]');
+        await driver.clickElement('.toggle-button');
+        await driver.clickElement('[data-testid="Localhost 8545"]');
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__subtitle',
+          text: `0x${sampleAddress2}`,
+        });
+      },
+    );
+  });
+});
